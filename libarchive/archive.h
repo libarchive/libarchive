5486b51d7d71a7573ef1b9cd8fd1842f03d45200
Joerg Sonnenberger <joerg.sonnenberger@gmail.com>
2008-10-01 10:07:50 -0400

    Introduce __LA_DEAD to replace current unportable use of __dead2 as discussed in August.
    SVN-Revision: 217

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -82,6 +82,13 @@
 # define __LA_DECL
 #endif
 
+#if defined(__GNUC__) && (__GNUC__ > 2 || \
+			  (__GNUC__ == 2 && __GNUC_MINOR__ >= 5))
+#define	__LA_DEAD	__attribute__((__noreturn__))
+#else
+#define	__LA_DEAD
+#endif
+
 
 #ifdef __cplusplus
 extern "C" {
