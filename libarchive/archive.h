84d5809db34f8e7ed6f4a0c655f437c8c846fdca
Tim Kientzle <kientzle@gmail.com>
2008-12-28 18:55:11 -0500

    Libarchive 2.6
    SVN-Revision: 295

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -113,13 +113,13 @@ extern "C" {
  *             (ARCHIVE_API_VERSION * 1000000 + ARCHIVE_API_FEATURE * 1000)
  * #endif
  */
-#define	ARCHIVE_VERSION_NUMBER 2005905
+#define	ARCHIVE_VERSION_NUMBER 2006000
 __LA_DECL int		archive_version_number(void);
 
 /*
  * Textual name/version of the library, useful for version displays.
  */
-#define	ARCHIVE_VERSION_STRING "libarchive 2.5.905a"
+#define	ARCHIVE_VERSION_STRING "libarchive 2.6.0"
 __LA_DECL const char *	archive_version_string(void);
 
 #if ARCHIVE_VERSION_NUMBER < 3000000
