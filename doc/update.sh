2039e354d9ce878a406c8a7797f05096528363cb
Tim Kientzle <kientzle@gmail.com>
2009-04-13 17:08:53 -0400

    First real attempt to convert all mdoc manpages to Wiki syntax. Including the awk script I'm using and updates to the doc generator to produce the Wiki versions as part of the regular deployment.
    SVN-Revision: 974

--- doc/update.sh
+++ doc/update.sh
@@ -5,24 +5,22 @@
 # the mdoc man pages stored in each project.
 #
 
-# Remove existing manpages from the doc tree
-chmod -R +w man text
-rm -f man/*.[135]
-rm -f text/*.txt
-rm -f pdf/*.pdf
+if false; then
 
 # Build Makefile in 'man' directory
 cd man
+rm -f *.[135]
 echo > Makefile
 echo "default: all" >>Makefile
 echo >>Makefile
 all="all:"
 for d in libarchive tar cpio; do
     for f in ../../$d/*.[135]; do
+	outname="`basename $f`"
 	echo >> Makefile
-	echo `basename $f`: ../mdoc2man.awk $f >> Makefile
-	echo "	awk -f ../mdoc2man.awk < $f > `basename $f`" >> Makefile
-        all="$all `basename $f`"
+	echo $outname: ../mdoc2man.awk $f >> Makefile
+	echo "	awk -f ../mdoc2man.awk < $f > $outname" >> Makefile
+        all="$all $outname"
     done
 done
 echo $all >>Makefile
@@ -30,16 +28,18 @@ cd ..
 
 # Rebuild Makefile in 'text' directory
 cd text
+rm -f *.txt
 echo > Makefile
 echo "default: all" >>Makefile
 echo >>Makefile
 all="all:"
 for d in libarchive tar cpio; do
     for f in ../../$d/*.[135]; do
+	outname="`basename $f`.txt"
 	echo >> Makefile
-	echo `basename $f`.txt: $f >> Makefile
-	echo "	nroff -mdoc $f | col -b > `basename $f`.txt" >> Makefile
-        all="$all `basename $f`.txt"
+	echo $outname: $f >> Makefile
+	echo "	nroff -mdoc $f | col -b > $outname" >> Makefile
+        all="$all $outname"
     done
 done
 echo $all >>Makefile
@@ -47,16 +47,58 @@ cd ..
 
 # Rebuild Makefile in 'pdf' directory
 cd pdf
+rm -f *.pdf
 echo > Makefile
 echo "default: all" >>Makefile
 echo >>Makefile
 all="all:"
 for d in libarchive tar cpio; do
     for f in ../../$d/*.[135]; do
+	outname="`basename $f`.pdf"
 	echo >> Makefile
-	echo `basename $f`.pdf: $f >> Makefile
-	echo "	groff -mdoc -T ps $f | ps2pdf - - > `basename $f`.pdf" >> Makefile
-        all="$all `basename $f`.pdf"
+	echo $outname: $f >> Makefile
+	echo "	groff -mdoc -T ps $f | ps2pdf - - > $outname" >> Makefile
+        all="$all $outname"
+    done
+done
+echo $all >>Makefile
+cd ..
+
+# Build Makefile in 'html' directory
+cd html
+rm -f *.html
+echo > Makefile
+echo "default: all" >>Makefile
+echo >>Makefile
+all="all:"
+for d in libarchive tar cpio; do
+    for f in ../../$d/*.[135]; do
+	outname="`basename $f`.html"
+	echo >> Makefile
+	echo $outname: ../mdoc2man.awk $f >> Makefile
+	echo "	groff -mdoc -T html $f > $outname" >> Makefile
+        all="$all $outname"
+    done
+done
+echo $all >>Makefile
+cd ..
+
+fi
+
+# Build Makefile in 'wiki' directory
+cd wiki
+rm -f *.wiki
+echo > Makefile
+echo "default: all" >>Makefile
+echo >>Makefile
+all="all:"
+for d in libarchive tar cpio; do
+    for f in ../../$d/*.[135]; do
+	outname="`basename $f | awk '{ac=split($0,a,"[_.-]");o="ManPage";for(w=0;w<=ac;++w){o=o toupper(substr(a[w],1,1)) substr(a[w],2)};print o}'`.wiki"
+	echo >> Makefile
+	echo $outname: ../mdoc2wiki.awk $f >> Makefile
+	echo "	awk -f ../mdoc2wiki.awk < $f > $outname" >> Makefile
+        all="$all $outname"
     done
 done
 echo $all >>Makefile
@@ -68,3 +110,7 @@ cd ..
 (cd text && make)
 # Format all of the manpages to PDF
 (cd pdf && make)
+# Format all of the manpages to HTML
+(cd html && make)
+# Format all of the manpages to Google Wiki syntax
+(cd wiki && make)
