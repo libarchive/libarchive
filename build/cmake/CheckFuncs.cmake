950a18b38ca9a090635f161348e8d279292c9ba2
Tim Kientzle <kientzle@gmail.com>
2009-08-02 16:00:46 -0400

    Rework the style of CHECK_FUNCS() to better match the style of built-in cmake tests:  * One test per invocation  * macro name is explicit
    Convert some of the tests to CHECK_SYMBOL_EXISTS(),
    which further reduces some of the magic within the
    old CHECK_FUNCS.  It is now primarily an extended
    version of CHECK_FUNCTION_EXISTS that understands the
    glibc __stub_ convention for marking functions that
    should be treated as nonexistent even though they
    do exist in the local library.  I've renamed the
    macro to CHECK_FUNCTION_EXISTS_GLIBC to better
    reflect this.
    
    SVN-Revision: 1308

--- build/cmake/CheckFuncs.cmake
+++ build/cmake/CheckFuncs.cmake
@@ -1,7 +1,15 @@
-# - Check if the system has the specified type
-# CHECK_FUNCS (FUNCTION1 FUNCTION2 ...)
+# Check if the system has the specified function; treat glibc "stub"
+# functions as nonexistent:
+# CHECK_FUNCTION_EXISTS_GLIBC (FUNCTION FUNCVAR)
 #
 #  FUNCTION - the function(s) where the prototype should be declared
+#  FUNCVAR - variable to define if the function does exist
+#
+# In particular, this understands the glibc convention of
+# defining macros __stub_XXXX or __stub___XXXX if the function
+# does appear in the library but is merely a stub that does nothing.
+# By detecting this case, we can select alternate behavior on
+# platforms that don't support this functionality.
 #
 # The following variables may be set before calling this macro to
 # modify the way the check is run:
@@ -15,41 +23,23 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 INCLUDE(CheckFunctionExists)
-INCLUDE(CheckSymbolExists)
 
-MACRO (CHECK_FUNCS _FUNCS)
-   FOREACH (_func ${ARGV})
-      STRING(TOUPPER ${_func} _funcvar)
-      SET(_funcvar "HAVE_${_funcvar}")
-      SET(_include "")
-      IF ("${_funcvar}" MATCHES "^${_funcvar}$")
-        IF (WIN32 AND "${_func}" MATCHES "^wmemcmp$")
-          SET(_include "wchar.h")
-        ENDIF (WIN32 AND "${_func}" MATCHES "^wmemcmp$")
-        IF (WIN32 AND "${_func}" MATCHES "^wmemcpy$")
-          SET(_include "wchar.h")
-        ENDIF (WIN32 AND "${_func}" MATCHES "^wmemcpy$")
-        IF ("${_include}" STREQUAL "")
-          SET(CHECK_STUB_FUNC_1 "__stub_${_func}")
-          SET(CHECK_STUB_FUNC_2 "__stub___${_func}")
-          CONFIGURE_FILE( ${CMAKE_SOURCE_DIR}/build/cmake/CheckFuncs_stub.c.in
-            ${CMAKE_BINARY_DIR}/cmake.tmp/CheckFuncs_stub.c IMMEDIATE)
-          TRY_COMPILE(__stub
-            ${CMAKE_BINARY_DIR}
-            ${CMAKE_BINARY_DIR}/cmake.tmp/CheckFuncs_stub.c
-            COMPILE_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS}
-            CMAKE_FLAGS
-            -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_INCLUDE_FILE_FLAGS}
-            "${CHECK_INCLUDE_FILE_C_INCLUDE_DIRS}")
-          IF (__stub)
-            SET("${_funcvar}" "" CACHE INTERNAL "Have function ${_func}")
-          ELSE (__stub)
-            CHECK_FUNCTION_EXISTS("${_func}" "${_funcvar}")
-          ENDIF (__stub)
-        ELSE ("${_include}" STREQUAL "")
-          CHECK_SYMBOL_EXISTS("${_func}" "${_include}" "${_funcvar}")
-        ENDIF ("${_include}" STREQUAL "")
-      ENDIF ("${_funcvar}" MATCHES "^${_funcvar}$")
-   ENDFOREACH (_func)
+MACRO (CHECK_FUNCTION_EXISTS_GLIBC _FUNC _FUNCVAR)
+   SET(CHECK_STUB_FUNC_1 "__stub_${_FUNC}")
+   SET(CHECK_STUB_FUNC_2 "__stub___${_FUNC}")
+   CONFIGURE_FILE( ${CMAKE_SOURCE_DIR}/build/cmake/CheckFuncs_stub.c.in
+     ${CMAKE_BINARY_DIR}/cmake.tmp/CheckFuncs_stub.c IMMEDIATE)
+   TRY_COMPILE(__stub
+     ${CMAKE_BINARY_DIR}
+     ${CMAKE_BINARY_DIR}/cmake.tmp/CheckFuncs_stub.c
+     COMPILE_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS}
+     CMAKE_FLAGS
+     -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_INCLUDE_FILE_FLAGS}
+     "${CHECK_INCLUDE_FILE_C_INCLUDE_DIRS}")
+   IF (__stub)
+     SET("${_FUNCVAR}" "" CACHE INTERNAL "Have function ${_FUNC}")
+   ELSE (__stub)
+     CHECK_FUNCTION_EXISTS("${_FUNC}" "${_FUNCVAR}")
+   ENDIF (__stub)
 ENDMACRO (CHECK_FUNCS)
 
