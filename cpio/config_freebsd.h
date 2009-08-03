5d5d8ed2a117a42f02f46f960c12dd5aed547709
Tim Kientzle <kientzle@gmail.com>
2009-08-02 22:23:10 -0400

    Minor FreeBSD build fix.
    SVN-Revision: 1326

--- cpio/config_freebsd.h
+++ cpio/config_freebsd.h
@@ -35,11 +35,15 @@
 #define	HAVE_FUTIMES 1
 #define	HAVE_GRP_H 1
 #define	HAVE_LIBARCHIVE 1
+#define	HAVE_LINK 1
+#define	HAVE_LSTAT 1
 #define	HAVE_LUTIMES 1
 #define	HAVE_PWD_H 1
+#define	HAVE_READLINK 1
 #define	HAVE_STDARG_H 1
 #define	HAVE_STDLIB_H 1
 #define	HAVE_STRING_H 1
+#define	HAVE_SYMLINK 1
 #define	HAVE_SYS_STAT_H 1
 #define	HAVE_SYS_TIME_H 1
 #define	HAVE_TIME_H 1
