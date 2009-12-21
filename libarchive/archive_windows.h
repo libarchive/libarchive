b40f003d10d151857d0aeb1555616782380d3723
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2009-12-21 03:51:19 -0500

    Avoid build failure on Visual Studio 10 W64.
    SVN-Revision: 1771

--- libarchive/archive_windows.h
+++ libarchive/archive_windows.h
@@ -54,6 +54,9 @@
 #define	set_errno(val)	((errno)=val)
 #include <io.h>
 #include <stdlib.h>   //brings in NULL
+#if defined(HAVE_STDINT_H)
+#include <stdint.h>
+#endif
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
