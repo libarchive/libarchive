57a2f651030ccd64d8f709471819932b6c25afa5
Tim Kientzle <kientzle@gmail.com>
2009-12-30 01:44:19 -0500

    Minor style fixes.
    SVN-Revision: 1785

--- libarchive/test/test_read_uu.c
+++ libarchive/test/test_read_uu.c
@@ -24,6 +24,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "test.h"
+__FBSDID("$FreeBSD: head/lib/libarchive/test/test_read_uu.c 201248 2009-12-30 06:12:03Z kientzle $");
 
 static const char archive[] = {
 "begin 644 test_read_uu.Z\n"
@@ -76,7 +77,6 @@ test_read_uu_sub(const char *uudata, size_t uusize)
 	char *buff;
 	int extra;
 
-	
 	assert(NULL != (buff = malloc(uusize + 64 * 1024)));
 	if (buff == NULL)
 		return;
@@ -101,7 +101,7 @@ test_read_uu_sub(const char *uudata, size_t uusize)
 		}
 		memcpy(p, uudata, uusize);
 		size = extra * 1024 + uusize;
-		
+
 		assert((a = archive_read_new()) != NULL);
 		assertEqualIntA(a, ARCHIVE_OK,
 		    archive_read_support_compression_all(a));
@@ -119,11 +119,7 @@ test_read_uu_sub(const char *uudata, size_t uusize)
 		    archive_format_name(a));
 		assertEqualInt(archive_format(a), ARCHIVE_FORMAT_TAR_USTAR);
 		assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
-#if ARCHIVE_VERSION_NUMBER < 2000000
-		archive_read_finish(a);
-#else
 		assertEqualInt(ARCHIVE_OK, archive_read_finish(a));
-#endif
 	}
 	free(buff);
 }
