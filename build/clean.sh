b2de49088e8749c4ec0a5c07b1084760273f8d48
Tim Kientzle <kientzle@gmail.com>
2010-01-16 04:20:22 -0500

    Sort the list of files to delete.
    SVN-Revision: 1826

--- build/clean.sh
+++ build/clean.sh
@@ -18,31 +18,64 @@ rm -rf /usr/obj`pwd`
 #
 # Try to clean up a bit more...
 #
-find . -name '*~' | xargs rm
-find . -name '*.rej' | xargs rm
-find . -name '*.orig' | xargs rm
-find . -name '*.o' | xargs rm
-find . -name '*.po' | xargs rm
-find . -name '*.lo' | xargs rm
+
 find . -name '*.So' | xargs rm
 find . -name '*.a' | xargs rm
 find . -name '*.la' | xargs rm
+find . -name '*.lo' | xargs rm
+find . -name '*.o' | xargs rm
+find . -name '*.orig' | xargs rm
+find . -name '*.po' | xargs rm
+find . -name '*.rej' | xargs rm
+find . -name '*~' | xargs rm
 find . -name '.depend' | xargs rm
-find . -name '.dirstamp' | xargs rm
 find . -name '.deps' | xargs rm -rf
+find . -name '.dirstamp' | xargs rm
 find . -name '.libs' | xargs rm -rf
+
 rm -rf autom4te.cache
-rm -f bsdcpio bsdcpio_test bsdtar bsdtar_test libarchive_test
-rm -f libtool aclocal.m4 config.h config.h.in
-rm -f doc/man/* doc/pdf/* doc/wiki/* doc/html/* doc/text/*
-rm -f config.log Makefile.in configure config.status stamp-h1
-rm -f libarchive/*.[35].gz libarchive/libarchive.so*
-rm -f libarchive/test/libarchive_test libarchive/test/list.h
-rm -f tar/*.1.gz tar/bsdtar tar/test/bsdtar_test tar/test/list.h
-rm -f cpio/*.1.gz cpio/bsdcpio cpio/test/bsdcpio_test cpio/test/list.h
-rm -f build/autoconf/compile build/autoconf/config.* build/autoconf/ltmain.sh
-rm -f build/autoconf/install-sh build/autoconf/depcomp build/autoconf/missing
-rm -f build/autoconf/libtool.m4 build/autoconf/ltoptions.m4
-rm -f build/autoconf/ltsugar.m4 build/autoconf/ltversion.m4
-rm -rf build/autoconf/lt~obsolete.m4
+
+rm -f Makefile.in
+rm -f aclocal.m4
+rm -f bsdcpio
+rm -f bsdcpio_test
+rm -f bsdtar
+rm -f bsdtar_test
+rm -f build/autoconf/compile
+rm -f build/autoconf/config.*
+rm -f build/autoconf/depcomp
+rm -f build/autoconf/install-sh
+rm -f build/autoconf/libtool.m4
+rm -f build/autoconf/ltmain.sh
+rm -f build/autoconf/ltoptions.m4
+rm -f build/autoconf/ltsugar.m4
+rm -f build/autoconf/ltversion.m4
+rm -f build/autoconf/lt~obsolete.m4
+rm -f build/autoconf/missing
 rm -f build/pkgconfig/libarchive.pc
+rm -f build/version.old
+rm -f config.h
+rm -f config.h.in
+rm -f config.log
+rm -f config.status
+rm -f configure
+rm -f cpio/*.1.gz
+rm -f cpio/bsdcpio
+rm -f cpio/test/bsdcpio_test
+rm -f cpio/test/list.h
+rm -f doc/html/*
+rm -f doc/man/*
+rm -f doc/pdf/*
+rm -f doc/text/*
+rm -f doc/wiki/*
+rm -f libarchive/*.[35].gz
+rm -f libarchive/libarchive.so*
+rm -f libarchive/test/libarchive_test
+rm -f libarchive/test/list.h
+rm -f libarchive_test
+rm -f libtool
+rm -f stamp-h1
+rm -f tar/*.1.gz
+rm -f tar/bsdtar
+rm -f tar/test/bsdtar_test
+rm -f tar/test/list.h
