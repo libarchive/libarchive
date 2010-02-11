ea455cd24c1e3da703724925afa7b91753579faf
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2010-02-11 00:50:35 -0500

    The last sparse block info must reach the end of file.
    SVN-Revision: 1894

--- libarchive/archive_write_set_format_pax.c
+++ libarchive/archive_write_set_format_pax.c
@@ -497,6 +497,21 @@ archive_write_pax_header(struct archive_write *a,
 		sparse_count = archive_entry_sparse_reset(entry_main);
 	else
 		sparse_count = 0;
+	if (sparse_count) {
+		int64_t offset, length, last_offset = 0;
+		/* Get the last entry of sparse block. */
+		while (archive_entry_sparse_next(
+		    entry_main, &offset, &length) == ARCHIVE_OK)
+			last_offset = offset + length;
+
+		/* If the last sparse block does not reach the end of file,
+		 * We have to add a empty sparse block as the last entry to
+		 * manage storing file data. */
+		if (last_offset < archive_entry_size(entry_main))
+			archive_entry_sparse_add_entry(entry_main,
+			    archive_entry_size(entry_main), 0);
+		sparse_count = archive_entry_sparse_reset(entry_main);
+	}
 
 	/*
 	 * First, check the name fields and see if any of them
