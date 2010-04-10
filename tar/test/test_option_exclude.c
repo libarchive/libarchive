8dc62cccb991920eff10999b51a34a878dcd85fb
Tim Kientzle <kientzle@gmail.com>
2010-04-10 15:49:56 -0400

    Add basic test for --exclude
    SVN-Revision: 2217

---
+++ tar/test/test_option_exclude.c
@@ -0,0 +1,84 @@
+/*-
+ * Copyright (c) 2010 Tim Kientzle
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "test.h"
+__FBSDID("$FreeBSD$");
+
+DEFINE_TEST(test_option_exclude)
+{
+	FILE *f;
+	int r;
+
+	f = fopen("file1", "w");
+	assert(f != NULL);
+	fprintf(f, "file1");
+	fclose(f);
+
+	f = fopen("file2", "w");
+	assert(f != NULL);
+	fprintf(f, "file2");
+	fclose(f);
+
+	assertEqualInt(0, systemf("%s -cf archive.tar file1 file2", testprog));
+
+	/*
+	 * Now, try extracting from the test archive with various --exclude options.
+	 */
+
+	/* Test 1: Without --exclude */
+	assertMakeDir("test1", 0755);
+	assertChdir("test1");
+	r = systemf("%s -xf ../archive.tar >test.out 2>test.err",
+	    testprog);
+	if (!assertEqualInt(0, r))
+		return;
+
+	assertFileContents("file1", 5, "file1");
+	assertFileContents("file2", 5, "file2");
+	assertEmptyFile("test.out");
+	assertEmptyFile("test.err");
+	assertChdir("..");
+
+	/* Test 2: Selecting just one file */
+	assertMakeDir("test2", 0755);
+	assertChdir("test2");
+	assertEqualInt(0,
+	    systemf("%s -xf ../archive.tar file1 >test.out 2>test.err", testprog));
+	assertFileContents("file1", 5, "file1");
+	assertFileNotExists("file2");
+	assertEmptyFile("test.out");
+	assertEmptyFile("test.err");
+	assertChdir("..");
+
+	/* Test 3: Use --exclude to skip one file */
+	assertMakeDir("test3", 0755);
+	assertChdir("test3");
+	assertEqualInt(0,
+	    systemf("%s -xf ../archive.tar --exclude file1 >test.out 2>test.err", testprog));
+	assertFileNotExists("file1");
+	assertFileContents("file2", 5, "file2");
+	assertEmptyFile("test.out");
+	assertEmptyFile("test.err");
+	assertChdir("..");
+}
