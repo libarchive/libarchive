f6f0f00f73af490eeca0c849f4f2c48775f4cf41
Tim Kientzle <kientzle@gmail.com>
2010-05-01 13:46:26 -0400

    Some Solaris systems seem to give root a gid of 1.
    SVN-Revision: 2347

--- cpio/test/test_owner_parse.c
+++ cpio/test/test_owner_parse.c
@@ -31,7 +31,7 @@ __FBSDID("$FreeBSD$");
 #if !defined(_WIN32)
 #define ROOT "root"
 static int root_uids[] = { 0 };
-static int root_gids[] = { 0 };
+static int root_gids[] = { 0, 1 };
 #elif defined(__CYGWIN__)
 /* On cygwin, the Administrator user most likely exists (unless
  * it has been renamed or is in a non-English localization), but
