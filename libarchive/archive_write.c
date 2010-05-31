55aec9a7b8f5a2b32a8dd6faa3cb2d24f10ce05c
Tim Kientzle <kientzle@gmail.com>
2010-05-31 14:20:46 -0400

    Handle EINTR in write calls:   * In low-level writers, just retry after EINTR   * In archive_write, handle short writes by generating additional writes for the remainder of the block.
    In particular, this fixes a problem with bsdtar failing if you try to use the SIGINT handler.
    
    SVN-Revision: 2431

--- libarchive/archive_write.c
+++ libarchive/archive_write.c
@@ -361,11 +361,21 @@ archive_write_client_write(struct archive_write_filter *f,
                 remaining -= to_copy;
                 /* ... if it's full, write it out. */
                 if (state->avail == 0) {
-                        bytes_written = (a->client_writer)(&a->archive,
-                            a->client_data, state->buffer, state->buffer_size);
-                        if (bytes_written <= 0)
-                                return (ARCHIVE_FATAL);
-                        /* XXX TODO: if bytes_written < state->buffer_size */
+			char *p = state->buffer;
+			size_t to_write = state->buffer_size;
+			while (to_write > 0) {
+				bytes_written = (a->client_writer)(&a->archive,
+				    a->client_data, p, to_write);
+				if (bytes_written <= 0)
+					return (ARCHIVE_FATAL);
+				if (bytes_written > to_write) {
+					archive_set_error(&(a->archive),
+					    -1, "write overrun");
+					return (ARCHIVE_FATAL);
+				}
+				p += bytes_written;
+				to_write -= bytes_written;
+			}
                         state->next = state->buffer;
                         state->avail = state->buffer_size;
                 }
