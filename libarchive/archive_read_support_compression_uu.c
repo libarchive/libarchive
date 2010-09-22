4c04f2303171423fec01b1b25969e8b5cd7e5c90
Brian Harring <ferringb@gmail.com>
2010-09-21 23:41:19 -0400

    fix out of bounds access; confirm the size, than do the access.
    SVN-Revision: 2677

--- libarchive/archive_read_support_compression_uu.c
+++ libarchive/archive_read_support_compression_uu.c
@@ -251,9 +251,9 @@ uudecode_bidder_bid(struct archive_read_filter_bidder *self,
 		len = bid_get_line(filter, &b, &avail, &ravail, &nl);
 		if (len < 0 || nl == 0)
 			return (0);/* Binary data. */
-		if (memcmp(b, "begin ", 6) == 0 && len - nl >= 11)
+		if (len - nl >= 11 && memcmp(b, "begin ", 6) == 0)
 			l = 6;
-		else if (memcmp(b, "begin-base64 ", 13) == 0 && len - nl >= 18)
+		else if (len -nl >= 18 && memcmp(b, "begin-base64 ", 13) == 0)
 			l = 13;
 		else
 			l = 0;
