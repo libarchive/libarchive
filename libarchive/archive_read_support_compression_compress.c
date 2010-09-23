87772ef98a29a20cd22f522a1fd50709775f9692
Brian Harring <ferringb@gmail.com>
2010-09-23 06:21:09 -0400

    correct a mismatch- you tell libarchive something is consumed only after you've actually consumed it.  Once you've consumed a read_ahead block, the data it points to is volatile.  Mainline trunk currently currently wouldn't cause issues with this usage, but the usage violates the api contract (and will have issues under libtransform) so fixing it.
    SVN-Revision: 2687

--- libarchive/archive_read_support_compression_compress.c
+++ libarchive/archive_read_support_compression_compress.c
@@ -95,6 +95,7 @@ struct private_data {
 	/* Input variables. */
 	const unsigned char	*next_in;
 	size_t			 avail_in;
+	size_t			 consume_unnotified;
 	int			 bit_buffer;
 	int			 bits_avail;
 	size_t			 bytes_in_section;
@@ -420,6 +421,11 @@ getbits(struct archive_read_filter *self, int n)
 
 	while (state->bits_avail < n) {
 		if (state->avail_in <= 0) {
+			if (state->consume_unnotified) {
+				__archive_read_filter_consume(self->upstream,
+					state->consume_unnotified);
+				state->consume_unnotified = 0;
+			}
 			state->next_in
 			    = __archive_read_filter_ahead(self->upstream,
 				1, &ret);
@@ -427,8 +433,7 @@ getbits(struct archive_read_filter *self, int n)
 				return (-1);
 			if (ret < 0 || state->next_in == NULL)
 				return (ARCHIVE_FATAL);
-			state->avail_in = ret;
-			__archive_read_filter_consume(self->upstream, ret);
+			state->consume_unnotified = state->avail_in = ret;
 		}
 		state->bit_buffer |= *state->next_in++ << state->bits_avail;
 		state->avail_in--;
