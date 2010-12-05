88d19ea4442c6b9cd5c74777d31facb9d764437e
Joerg Sonnenberger <joerg.sonnenberger@gmail.com>
2010-12-04 19:54:03 -0500

    Adjust cut-off condition to correctly return INT_MAX for large integers. Found by Maksymilian Arciemowicz <cxib@securityreason.com>.
    SVN-Revision: 2810

--- libarchive/archive_acl.c
+++ libarchive/archive_acl.c
@@ -626,9 +626,10 @@ isint_w(const wchar_t *start, const wchar_t *end, int *result)
 	while (start < end) {
 		if (*start < '0' || *start > '9')
 			return (0);
-		if (n > (INT_MAX / 10))
+		if (n > (INT_MAX / 10) ||
+		    (n == INT_MAX / 10 && (*start - '0') > INT_MAX % 10)) {
 			n = INT_MAX;
-		else {
+		} else {
 			n *= 10;
 			n += *start - '0';
 		}
