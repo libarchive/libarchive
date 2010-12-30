64fca92bcb0a9e1088721bde1259dc317ddc4903
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2010-12-30 03:18:57 -0500

    Prevent inifity loop when reading broken CAB file.
    SVN-Revision: 2840

--- libarchive/archive_read_support_format_cab.c
+++ libarchive/archive_read_support_format_cab.c
@@ -1840,7 +1840,14 @@ cab_read_data(struct archive_read *a, const void **buff,
 		*buff = NULL;
 		*size = 0;
 		*offset = 0;
-		return (bytes_avail);
+		if (bytes_avail == 0 &&
+		    cab->entry_cfdata->uncompressed_size == 0) {
+			/* All of CFDATA in a folder has been handled. */
+			archive_set_error(&a->archive,
+			    ARCHIVE_ERRNO_FILE_FORMAT, "Invalid CFDATA");
+			return (ARCHIVE_FATAL);
+		} else
+			return (bytes_avail);
 	}
 	if (bytes_avail > cab->entry_bytes_remaining)
 		bytes_avail = cab->entry_bytes_remaining;
