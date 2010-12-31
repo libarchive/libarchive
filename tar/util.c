a7b900bd075c8bf5bb6904610c82a4db921f62ed
Tim Kientzle <kientzle@gmail.com>
2010-12-31 00:09:18 -0500

    Correctly format the value -9223372036854775808.
    SVN-Revision: 2845

--- tar/util.c
+++ tar/util.c
@@ -503,14 +503,13 @@ const char *
 tar_i64toa(int64_t n0)
 {
 	static char buff[24];
-	int64_t n = n0 < 0 ? -n0 : n0;
+	uint64_t n = n0 < 0 ? -n0 : n0;
 	char *p = buff + sizeof(buff);
 
 	*--p = '\0';
 	do {
 		*--p = '0' + (int)(n % 10);
-		n /= 10;
-	} while (n > 0);
+	} while (n /= 10);
 	if (n0 < 0)
 		*--p = '-';
 	return p;
