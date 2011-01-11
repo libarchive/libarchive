02a4f67e1bc51fc0f589c943d3b5f0831d685857
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-01-11 11:35:59 -0500

    Update a test for directory traversals;  1. add a test for archive_read_disk_set_symlink_hybrid(),  2. add a test for archive_read_disk_set_symlink_logical().
    SVN-Revision: 2875

--- libarchive/test/test_read_disk_directory_traversals.c
+++ libarchive/test/test_read_disk_directory_traversals.c
@@ -31,7 +31,8 @@ __FBSDID("$FreeBSD$");
 # endif
 #endif
 
-DEFINE_TEST(test_read_disk_directory_traversals)
+static void
+test_basic()
 {
 	struct archive *a;
 	struct archive_entry *ae;
@@ -276,4 +277,401 @@ DEFINE_TEST(test_read_disk_directory_traversals)
 	free(cwd);
 
 	archive_entry_free(ae);
+}
+
+static void
+test_symlink_hybrid()
+{
+	struct archive *a;
+	struct archive_entry *ae;
+	const void *p;
+	size_t size;
+	int64_t offset;
+	int file_count;
+
+	if (!canSymlink()) {
+		skipping("Can't test symlinks on this filesystem");
+		return;
+	}
+
+	/*
+	 * Create a sample archive.
+	 */
+	assertMakeDir("h", 0755);
+	assertChdir("h");
+	assertMakeDir("d1", 0755);
+	assertMakeSymlink("ld1", "d1");
+	assertMakeFile("d1/file1", 0644, "d1/file1");
+	assertMakeFile("d1/file2", 0644, "d1/file2");
+	assertMakeSymlink("d1/link1", "file1");
+	assertMakeSymlink("d1/linkX", "fileX");
+	assertMakeSymlink("link2", "d1/file2");
+	assertMakeSymlink("linkY", "d1/fileY");
+	assertChdir("..");
+
+	assert((ae = archive_entry_new()) != NULL);
+	assert((a = archive_read_disk_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK,
+	    archive_read_disk_set_symlink_hybrid(a));
+
+	/*
+	 * Specified file is a symbolic link file.
+	 */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_disk_open(a, "h/ld1"));
+	file_count = 5;
+
+	while (file_count--) {
+		assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header2(a, ae));
+		if (strcmp(archive_entry_pathname(ae), "h/ld1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/ld1/file1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/ld1/file2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file2", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/ld1/link1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/ld1/linkX") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		}
+		if (archive_entry_filetype(ae) == AE_IFDIR) {
+			/* Descend into the current object */
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_disk_descend(a));
+		}
+	}
+	/* There is no entry. */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header2(a, ae));
+	/* Close the disk object. */
+	assertEqualInt(ARCHIVE_OK, archive_read_close(a));
+
+	/*
+	 * Specified file is a directory and it has symbolic files.
+	 */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_disk_open(a, "h"));
+	file_count = 9;
+
+	while (file_count--) {
+		assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header2(a, ae));
+		if (strcmp(archive_entry_pathname(ae), "h") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae), "h/d1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/d1/file1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/d1/file2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file2", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae), "h/ld1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/d1/link1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/d1/linkX") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/link2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "h/linkY") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		}
+		if (archive_entry_filetype(ae) == AE_IFDIR) {
+			/* Descend into the current object */
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_disk_descend(a));
+		}
+	}
+	/* There is no entry. */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header2(a, ae));
+	/* Close the disk object. */
+	assertEqualInt(ARCHIVE_OK, archive_read_close(a));
+	/* Destroy the disk object. */
+	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
+	archive_entry_free(ae);
+}
+
+static void
+test_symlink_logical()
+{
+	struct archive *a;
+	struct archive_entry *ae;
+	const void *p;
+	size_t size;
+	int64_t offset;
+	int file_count;
+
+	if (!canSymlink()) {
+		skipping("Can't test symlinks on this filesystem");
+		return;
+	}
+
+	/*
+	 * Create a sample archive.
+	 */
+	assertMakeDir("l", 0755);
+	assertChdir("l");
+	assertMakeDir("d1", 0755);
+	assertMakeSymlink("ld1", "d1");
+	assertMakeFile("d1/file1", 0644, "d1/file1");
+	assertMakeFile("d1/file2", 0644, "d1/file2");
+	assertMakeSymlink("d1/link1", "file1");
+	assertMakeSymlink("d1/linkX", "fileX");
+	assertMakeSymlink("link2", "d1/file2");
+	assertMakeSymlink("linkY", "d1/fileY");
+	assertChdir("..");
+
+	assert((ae = archive_entry_new()) != NULL);
+	assert((a = archive_read_disk_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK,
+	    archive_read_disk_set_symlink_logical(a));
+
+	/*
+	 * Specified file is a symbolic link file.
+	 */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_disk_open(a, "l/ld1"));
+	file_count = 5;
+
+	while (file_count--) {
+		assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header2(a, ae));
+		if (strcmp(archive_entry_pathname(ae), "l/ld1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/file1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/file2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file2", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/link1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/linkX") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		}
+		if (archive_entry_filetype(ae) == AE_IFDIR) {
+			/* Descend into the current object */
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_disk_descend(a));
+		}
+	}
+	/* There is no entry. */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header2(a, ae));
+	/* Close the disk object. */
+	assertEqualInt(ARCHIVE_OK, archive_read_close(a));
+
+	/*
+	 * Specified file is a directory and it has symbolic files.
+	 */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_disk_open(a, "l"));
+	file_count = 13;
+
+	while (file_count--) {
+		assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header2(a, ae));
+		if (strcmp(archive_entry_pathname(ae), "l") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae), "l/d1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/d1/file1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/d1/file2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file2", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/d1/link1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/d1/linkX") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae), "l/ld1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFDIR);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/file1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/file2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file2", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/link1") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file1", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/ld1/linkX") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/link2") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFREG);
+			assertEqualInt(archive_entry_size(ae), 8);
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 8);
+			assertEqualInt((int)offset, 0);
+			assertEqualInt(memcmp(p, "d1/file2", 8), 0);
+			assertEqualInt(ARCHIVE_EOF,
+			    archive_read_data_block(a, &p, &size, &offset));
+			assertEqualInt((int)size, 0);
+			assertEqualInt((int)offset, 8);
+		} else if (strcmp(archive_entry_pathname(ae),
+		    "l/linkY") == 0) {
+			assertEqualInt(archive_entry_filetype(ae), AE_IFLNK);
+		}
+		if (archive_entry_filetype(ae) == AE_IFDIR) {
+			/* Descend into the current object */
+			assertEqualIntA(a, ARCHIVE_OK,
+			    archive_read_disk_descend(a));
+		}
+	}
+	/* There is no entry. */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header2(a, ae));
+	/* Close the disk object. */
+	assertEqualInt(ARCHIVE_OK, archive_read_close(a));
+	/* Destroy the disk object. */
+	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
+	archive_entry_free(ae);
+}
+
+DEFINE_TEST(test_read_disk_directory_traversals)
+{
+	/* Basic test. */
+	test_basic();
+	/* Test hybird mode; follow symlink initially, then not. */
+	test_symlink_hybrid();
+	/* Test logcal mode; follow all symlinks. */
+	test_symlink_logical();
 }
