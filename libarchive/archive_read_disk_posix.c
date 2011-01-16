54a4c9b72e53744955fd4274205d89e06ec1e68a
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-01-16 18:16:08 -0500

    Remove the wrong initialization which caused invalid memory access.
    SVN-Revision: 2899

--- libarchive/archive_read_disk_posix.c
+++ libarchive/archive_read_disk_posix.c
@@ -1473,7 +1473,6 @@ tree_reopen(struct tree *t, const char *path)
 	t->descend = 0;
 	t->current = NULL;
 	t->d = INVALID_DIR_HANDLE;
-	t->current_filesystem_id = -1;
 	t->symlink_mode = t->initial_symlink_mode;
 	archive_string_empty(&t->path);
 
