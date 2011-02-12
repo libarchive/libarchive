3951b76425d0ef88b07693865cee620cb86a5223
Tim Kientzle <kientzle@gmail.com>
2011-02-12 18:51:36 -0500

    Issue 135: Have archive_write_data_block fail with an error instead of a segv if the operation isn't supported.
    SVN-Revision: 2978

--- libarchive/archive_virtual.c
+++ libarchive/archive_virtual.c
@@ -118,6 +118,12 @@ archive_write_data(struct archive *a, const void *buff, size_t s)
 ssize_t
 archive_write_data_block(struct archive *a, const void *buff, size_t s, int64_t o)
 {
+	if (a->vtable->archive_write_data_block == NULL) {
+		archive_set_error(a, ARCHIVE_ERRNO_MISC,
+		    "archive_write_data_block not supported");
+		a->state = ARCHIVE_STATE_FATAL;
+		return (ARCHIVE_FATAL);
+	}
 	return ((a->vtable->archive_write_data_block)(a, buff, s, o));
 }
 
