88b0f1779a4256c23cb2113bb63233a1cc9ca365
Tim Kientzle <kientzle@gmail.com>
2011-03-11 00:34:27 -0500

    Add a test of EUC-JP character encoding.
    SVN-Revision: 3005

--- libarchive/test/test_pax_filename_encoding_freebsd.c
+++ libarchive/test/test_pax_filename_encoding_freebsd.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2003-2007 Tim Kientzle
+ * Copyright (c) 2003-2011 Tim Kientzle
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,17 +23,17 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "test.h"
-__FBSDID("$FreeBSD: head/lib/libarchive/test/test_pax_filename_encoding.c 201247 2009-12-30 05:59:21Z kientzle $");
+__FBSDID("$FreeBSD$");
 
 #include <locale.h>
 
 /*
  * Pax interchange is supposed to encode filenames into
- * UTF-8.  This verifies that behavior on FreeBSD using
- * the KOI8-R locale.
+ * UTF-8.
  */
 
-DEFINE_TEST(test_pax_filename_encoding_freebsd)
+static void
+test_pax_filename_encoding_ru_RU()
 {
   	struct archive *a;
   	struct archive_entry *entry;
@@ -49,7 +49,7 @@ DEFINE_TEST(test_pax_filename_encoding_freebsd)
 	assertEqualInt(ARCHIVE_OK, archive_write_set_format_pax(a));
 	assertEqualInt(ARCHIVE_OK, archive_write_open_memory(a, buff, sizeof(buff), &used));
 
-	entry = archive_entry_new();
+	entry = archive_entry_new2(a);
 	archive_entry_set_pathname(entry, "\xD0\xD2\xC9");
 	archive_entry_set_filetype(entry, AE_IFREG);
 	archive_entry_set_size(entry, 0);
@@ -60,4 +60,43 @@ DEFINE_TEST(test_pax_filename_encoding_freebsd)
 	/* Above three characters in KOI8-R should translate to the following
 	 * three characters (two bytes each) in UTF-8. */
 	assertEqualMem(buff + 512, "15 path=\xD0\xBF\xD1\x80\xD0\xB8\x0A", 15);
+}
+
+static void
+test_pax_filename_encoding_ja_JP()
+{
+  	struct archive *a;
+  	struct archive_entry *entry;
+	char buff[4096];
+	wchar_t ws[] = {0x8868, L'.', L't', L'x', L't', 0};
+	size_t used;
+
+	if (NULL == setlocale(LC_ALL, "ja_JP.eucJP")) {
+		skipping("eucJP locale not available on this system.");
+		return;
+	}
+
+	a = archive_write_new();
+	assertEqualInt(ARCHIVE_OK, archive_write_set_format_pax(a));
+	assertEqualInt(ARCHIVE_OK, archive_write_open_memory(a, buff, sizeof(buff), &used));
+
+	entry = archive_entry_new2(a);
+	archive_entry_set_pathname(entry, "\xC9\xBD.txt");
+	/* Check the Unicode version. */
+	assertEqualMem(ws, archive_entry_pathname_w(entry), sizeof(ws));
+	archive_entry_set_filetype(entry, AE_IFREG);
+	archive_entry_set_size(entry, 0);
+	assertEqualInt(ARCHIVE_OK, archive_write_header(a, entry));
+	archive_entry_free(entry);
+	assertEqualInt(ARCHIVE_OK, archive_write_free(a));
+
+	/* Check UTF-8 version. */
+	assertEqualMem(buff + 512, "16 path=\xE8\xA1\xA8.txt\x0A", 16);
+
+}
+
+DEFINE_TEST(test_pax_filename_encoding_freebsd)
+{
+	test_pax_filename_encoding_ru_RU();
+	test_pax_filename_encoding_ja_JP();
 }
