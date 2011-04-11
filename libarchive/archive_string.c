db176ac31f35650323a1e75e4b6e786650673ed4
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-04-11 00:03:09 -0400

    Correctly convert charset names, both "CP_ACP" and "CP_OEMCP", to current ACP and OEMCP.
    SVN-Revision: 3205

--- libarchive/archive_string.c
+++ libarchive/archive_string.c
@@ -870,9 +870,9 @@ make_codepage_from_charset(const char *charset)
 		if (cs[1] == 'P' && cs[2] >= '0' && cs[2] <= '9') {
 			cp = my_atoi(cs + 2);
 		} else if (strcmp(cs, "CP_ACP") == 0)
-			cp = GetACP();
+			cp = get_current_codepage();
 		else if (strcmp(cs, "CP_OEMCP") == 0)
-			cp = GetOEMCP();
+			cp = get_current_oemcp();
 		break;
 	case 'I':
 		if (cs[1] == 'B' && cs[2] == 'M' &&
