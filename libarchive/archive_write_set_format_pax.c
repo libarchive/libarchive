b946208031f4f50fa9cd237423a812c6287930be
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-04-18 05:42:02 -0400

    Style fix.
    SVN-Revision: 3253

--- libarchive/archive_write_set_format_pax.c
+++ libarchive/archive_write_set_format_pax.c
@@ -176,9 +176,9 @@ archive_write_pax_options(struct archive_write *a, const char *key,
 		} else if (strcmp(val, "UTF-8") == 0) {
 			/*
 			 * Specify UTF-8 character-set to be used for filenames.
-			 * This is almost the test that running platform supports the string
-			 * conversion. Especially libarchive_test needs this trick for
-			 * its test.
+			 * This is almost the test that running platform supports
+			 * the string conversion. Especially libarchive_test needs
+			 * this trick for its test.
 			 */
 			pax->sconv_utf8 = archive_string_conversion_to_charset(
 			    &(a->archive), "UTF-8", 0);
