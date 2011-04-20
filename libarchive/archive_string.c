57ee447d9be91c363444a38c97236aa788fe5155
Tim Kientzle <kientzle@gmail.com>
2011-04-20 02:18:46 -0400

    Apparently, locale_charset() is broken on Mac OS.
    SVN-Revision: 3258

--- libarchive/archive_string.c
+++ libarchive/archive_string.c
@@ -319,7 +319,8 @@ static const char *
 default_iconv_charset(const char *charset) {
 	if (charset != NULL && charset[0] != '\0')
 		return charset;
-#if HAVE_LOCALE_CHARSET
+#if HAVE_LOCALE_CHARSET && !defined(__APPLE__)
+	/* locale_charset() is broken on Mac OS */
 	return locale_charset();
 #elif HAVE_NL_LANGINFO
 	return nl_langinfo(CODESET);
