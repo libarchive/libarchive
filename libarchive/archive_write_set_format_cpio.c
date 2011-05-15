0f6dae5a91aa7eb12d93a02263676b8ef57f1f77
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-05-15 11:21:42 -0400

    Remove unneeded code in archive_write_set_format_cpio.c
    SVN-Revision: 3335

--- libarchive/archive_write_set_format_cpio.c
+++ libarchive/archive_write_set_format_cpio.c
@@ -234,7 +234,6 @@ get_sconv(struct archive_write *a)
 static int
 archive_write_cpio_header(struct archive_write *a, struct archive_entry *entry)
 {
-	struct cpio *cpio;
 	const char *path;
 	size_t len;
 
@@ -243,7 +242,6 @@ archive_write_cpio_header(struct archive_write *a, struct archive_entry *entry)
 		return (ARCHIVE_FAILED);
 	}
 
-	cpio = (struct cpio *)a->format_data;
 	(void)archive_entry_pathname_l(entry, &path, &len, get_sconv(a));
 	if (len == 0 || path == NULL || path[0] == '\0') {
 		archive_set_error(&a->archive, -1, "Pathname required");
