b338a1213430b248e0fda8e105073fca27fa7e05
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-08-03 01:13:37 -0400

    Fix the uu filter. That did not extract uuencoded data when an output file-name is very short.
    SVN-Revision: 3519

--- libarchive/archive_read_support_filter_uu.c
+++ libarchive/archive_read_support_filter_uu.c
@@ -528,9 +528,9 @@ read_more:
 				    "Invalid format data");
 				return (ARCHIVE_FATAL);
 			}
-			if (len - nl > 13 && memcmp(b, "begin ", 6) == 0)
+			if (len - nl >= 11 && memcmp(b, "begin ", 6) == 0)
 				l = 6;
-			else if (len - nl > 18 &&
+			else if (len - nl >= 18 &&
 			    memcmp(b, "begin-base64 ", 13) == 0)
 				l = 13;
 			else
