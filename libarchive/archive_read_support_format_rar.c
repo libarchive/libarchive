cc4135f3ebff0142fcfb178c52052c12a992c7c3
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-08-09 04:06:39 -0400

    Improve lzss_emit_match() to be faster than previous version. This cahnge makes user CPU time less about 40% when extracting test_read_format_rar_ppmd_lzss_conversion.rar.uu. This idea from our lha reader.
    SVN-Revision: 3576

--- libarchive/archive_read_support_format_rar.c
+++ libarchive/archive_read_support_format_rar.c
@@ -521,11 +521,33 @@ lzss_emit_literal(struct rar *rar, uint8_t literal)
 static inline void
 lzss_emit_match(struct rar *rar, int offset, int length)
 {
-  int i, windowoffs = lzss_current_offset(&rar->lzss);
-  for(i = 0; i < length; i++)
-  {
-    rar->lzss.window[(windowoffs + i) & lzss_mask(&rar->lzss)] =
-      rar->lzss.window[(windowoffs + i - offset) & lzss_mask(&rar->lzss)];
+  int dstoffs = lzss_current_offset(&rar->lzss);
+  int srcoffs = (dstoffs - offset) & lzss_mask(&rar->lzss);
+  int l, li, remaining;
+  unsigned char *d, *s;
+
+  remaining = length;
+  while (remaining > 0) {
+    l = remaining;
+    if (dstoffs > srcoffs) {
+      if (l > lzss_size(&rar->lzss) - dstoffs)
+        l = lzss_size(&rar->lzss) - dstoffs;
+    } else {
+      if (l > lzss_size(&rar->lzss) - srcoffs)
+        l = lzss_size(&rar->lzss) - srcoffs;
+    }
+    d = &(rar->lzss.window[dstoffs]);
+    s = &(rar->lzss.window[srcoffs]);
+    if (l >= 8 && ((dstoffs + l < srcoffs) || (srcoffs + l < dstoffs)))
+      memcpy(d, s, l);
+    else {
+      li = l;
+      while (li-- > 0)
+        *d++ = *s++;
+    }
+    remaining -= l;
+    dstoffs = (dstoffs + l) & lzss_mask(&(rar->lzss));
+    srcoffs = (srcoffs + l) & lzss_mask(&(rar->lzss));
   }
   rar->lzss.position += length;
 }
