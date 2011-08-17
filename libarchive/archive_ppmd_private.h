d3ec71683862f8e55de8302a8298e007f4ba4864
Tim Kientzle <kientzle@gmail.com>
2011-08-17 02:11:16 -0400

    Try to workaround the fact that zconf.h defines Byte also.
    SVN-Revision: 3636

--- libarchive/archive_ppmd_private.h
+++ libarchive/archive_ppmd_private.h
@@ -15,7 +15,9 @@ This code is based on PPMd var.H (2001): Dmitry Shkarin : Public domain */
 
 /*** Begin defined in Types.h ***/
 
+#if !defined(ZCONF_H)
 typedef unsigned char Byte;
+#endif
 typedef short Int16;
 typedef unsigned short UInt16;
 
