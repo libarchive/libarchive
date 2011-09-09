3da1f6c592508c77194adda9c53114e3c5c95c0a
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-09-09 00:29:25 -0400

    Add support for extracting a RAR SFX file.
    SVN-Revision: 3694

--- libarchive/archive_read_support_format_rar.c
+++ libarchive/archive_read_support_format_rar.c
@@ -242,6 +242,7 @@ struct rar
   char start_new_block;
   char entry_eof;
   unsigned long crc_calculated;
+  int found_first_header;
 
   /* LZSS members */
   struct huffman_code maincode;
@@ -659,7 +660,77 @@ archive_read_format_rar_bid(struct archive_read *a)
   if (memcmp(p, RAR_SIGNATURE, 7) == 0)
     return (30);
 
+  if (p[0] == 'M' && p[1] == 'Z') {
+    /* This is a PE file */
+    const void *buff;
+    ssize_t offset = 0x10000;
+    ssize_t window = 4096;
+    ssize_t bytes_avail;
+    while (offset + window <= (1024 * 128)) {
+      buff = __archive_read_ahead(a, offset + window, &bytes_avail);
+      if (buff == NULL) {
+        /* Remaining bytes are less than window. */
+        window >>= 1;
+        if (window < 0x40)
+          return (0);
+        continue;
+      }
+      p = (const char *)buff + offset;
+      while (p + 7 < (const char *)buff + bytes_avail) {
+        if (memcmp(p, RAR_SIGNATURE, 7) == 0)
+          return (30);
+        p += 0x100;
+      }
+      offset = p - (const char *)buff;
+    }
+  }
   return (0);
+}
+
+static int
+skip_sfx(struct archive_read *a)
+{
+  const void *h;
+  const char *p, *q;
+  size_t skip, total;
+  ssize_t bytes, window;
+
+  total = 0;
+  window = 4096;
+  while (total + window <= (1024 * 128)) {
+    h = __archive_read_ahead(a, window, &bytes);
+    if (h == NULL) {
+      /* Remaining bytes are less than window. */
+      window >>= 1;
+      if (window < 0x40)
+      	goto fatal;
+      continue;
+    }
+    if (bytes < 0x40)
+      goto fatal;
+    p = h;
+    q = p + bytes;
+
+    /*
+     * Scan ahead until we find something that looks
+     * like the RAR header.
+     */
+    while (p + 7 < q) {
+      if (memcmp(p, RAR_SIGNATURE, 7) == 0) {
+      	skip = p - (const char *)h;
+      	__archive_read_consume(a, skip);
+      	return (ARCHIVE_OK);
+      }
+      p += 0x100;
+    }
+    skip = p - (const char *)h;
+    __archive_read_consume(a, skip);
+	total += skip;
+  }
+fatal:
+  archive_set_error(&a->archive, ARCHIVE_ERRNO_FILE_FORMAT,
+      "Couldn't find out RAR header");
+  return (ARCHIVE_FATAL);
 }
 
 static int
@@ -711,8 +782,17 @@ archive_read_format_rar_read_header(struct archive_read *a,
   /* RAR files can be generated without EOF headers, so return ARCHIVE_EOF if
   * this fails.
   */
-  if (__archive_read_ahead(a, 7, NULL) == NULL)
+  if ((h = __archive_read_ahead(a, 7, NULL)) == NULL)
     return (ARCHIVE_EOF);
+
+  p = h;
+  if (rar->found_first_header == 0 && p[0] == 'M' && p[1] == 'Z') {
+    /* This is an executable ? Must be self-extracting... */
+    ret = skip_sfx(a);
+    if (ret < ARCHIVE_WARN)
+      return (ret);
+  }
+  rar->found_first_header = 1;
 
   while (1)
   {
