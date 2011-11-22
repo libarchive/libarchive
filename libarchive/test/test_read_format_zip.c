cf5fe037b8c4e4215795781974111d09b47a4b15
Tim Kientzle <kientzle@gmail.com>
2011-11-22 01:27:48 -0500

    Refactor Zip read tests to exercise both seeking and streaming readers.
    SVN-Revision: 3831

--- libarchive/test/test_read_format_zip.c
+++ libarchive/test/test_read_format_zip.c
@@ -37,33 +37,30 @@ static const int libz_enabled = 0;
  *   * file2 has length-at-end but file1 does not
  *   * file2 has an invalid CRC
  */
-
 static void
-test_basic(void)
+verify_basic(struct archive *a, int check_mode)
 {
-	const char *refname = "test_read_format_zip.zip";
 	struct archive_entry *ae;
-	struct archive *a;
 	char *buff[128];
 	const void *pv;
 	size_t s;
 	int64_t o;
 
-	extract_reference_file(refname);
-	assert((a = archive_read_new()) != NULL);
-	assertA(0 == archive_read_support_filter_all(a));
-	assertA(0 == archive_read_support_format_all(a));
-	assertA(0 == archive_read_open_filename(a, refname, 10240));
-	assertA(0 == archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
 	assertEqualString("dir/", archive_entry_pathname(ae));
 	assertEqualInt(1179604249, archive_entry_mtime(ae));
 	assertEqualInt(0, archive_entry_size(ae));
+	if (check_mode)
+		assertEqualInt(AE_IFDIR | 0755, archive_entry_mode(ae));
 	assertEqualIntA(a, ARCHIVE_EOF,
 	    archive_read_data_block(a, &pv, &s, &o));
 	assertEqualInt((int)s, 0);
-	assertA(0 == archive_read_next_header(a, &ae));
+
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
 	assertEqualString("file1", archive_entry_pathname(ae));
 	assertEqualInt(1179604289, archive_entry_mtime(ae));
+	if (check_mode)
+		assertEqualInt(AE_IFREG | 0755, archive_entry_mode(ae));
 	assertEqualInt(18, archive_entry_size(ae));
 	failure("archive_read_data() returns number of bytes read");
 	if (libz_enabled) {
@@ -75,9 +72,12 @@ test_basic(void)
 		    "libarchive compiled without deflate support (no libz)");
 		assert(archive_errno(a) != 0);
 	}
-	assertA(0 == archive_read_next_header(a, &ae));
+
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
 	assertEqualString("file2", archive_entry_pathname(ae));
 	assertEqualInt(1179605932, archive_entry_mtime(ae));
+	if (check_mode)
+		assertEqualInt(AE_IFREG | 0755, archive_entry_mode(ae));
 	failure("file2 has length-at-end, so we shouldn't see a valid size");
 	assertEqualInt(0, archive_entry_size_is_set(ae));
 	if (libz_enabled) {
@@ -94,10 +94,36 @@ test_basic(void)
 	/* Verify the number of files read. */
 	failure("the archive file has three files");
 	assertEqualInt(3, archive_file_count(a));
-	assertA(archive_compression(a) == ARCHIVE_COMPRESSION_NONE);
-	assertA(archive_format(a) == ARCHIVE_FORMAT_ZIP);
+	assertEqualIntA(a, ARCHIVE_COMPRESSION_NONE, archive_compression(a));
+	assertEqualIntA(a, ARCHIVE_FORMAT_ZIP, archive_format(a));
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
 	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
+}
+
+static void
+test_basic(void)
+{
+	const char *refname = "test_read_format_zip.zip";
+	struct archive *a;
+	char *p;
+	size_t s;
+
+	extract_reference_file(refname);
+
+	/* Verify with seeking reader. */
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_open_filename(a, refname, 10240));
+	verify_basic(a, 1);
+
+	/* Verify with streaming reader. */
+	p = slurpfile(&s, refname);
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, read_open_memory(a, p, s, 31));
+	verify_basic(a, 0);
 }
 
 /*
@@ -105,23 +131,17 @@ test_basic(void)
  *  Currently stores Unix UID/GID up to 32 bits.
  */
 static void
-test_info_zip_ux(void)
+verify_info_zip_ux(struct archive *a, int check_mode)
 {
-	const char *refname = "test_read_format_zip_ux.zip";
 	struct archive_entry *ae;
-	struct archive *a;
 	char *buff[128];
 
-	extract_reference_file(refname);
-	assert((a = archive_read_new()) != NULL);
-	assertA(0 == archive_read_support_filter_all(a));
-	assertA(0 == archive_read_support_format_all(a));
-	assertA(0 == archive_read_open_filename(a, refname, 10240));
-
-	assertA(0 == archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
 	assertEqualString("file1", archive_entry_pathname(ae));
 	assertEqualInt(1300668680, archive_entry_mtime(ae));
 	assertEqualInt(18, archive_entry_size(ae));
+	if (check_mode)
+		assertEqualInt(AE_IFREG | 0644, archive_entry_mode(ae));
 	failure("zip reader should read Info-ZIP New Unix Extra Field");
 	assertEqualInt(1001, archive_entry_uid(ae));
 	assertEqualInt(1001, archive_entry_gid(ae));
@@ -141,10 +161,36 @@ test_info_zip_ux(void)
 	failure("the archive file has just one file");
 	assertEqualInt(1, archive_file_count(a));
 
-	assertA(archive_compression(a) == ARCHIVE_COMPRESSION_NONE);
-	assertA(archive_format(a) == ARCHIVE_FORMAT_ZIP);
+	assertEqualIntA(a, ARCHIVE_COMPRESSION_NONE, archive_compression(a));
+	assertEqualIntA(a, ARCHIVE_FORMAT_ZIP, archive_format(a));
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
 	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
+}
+
+static void
+test_info_zip_ux(void)
+{
+	const char *refname = "test_read_format_zip_ux.zip";
+	struct archive *a;
+	char *p;
+	size_t s;
+
+	extract_reference_file(refname);
+
+	/* Verify with seeking reader. */
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_open_filename(a, refname, 10240));
+	verify_info_zip_ux(a, 1);
+
+	/* Verify with streaming reader. */
+	p = slurpfile(&s, refname);
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, read_open_memory(a, p, s, 108));
+	verify_info_zip_ux(a, 0);
 }
 
 /*
@@ -152,23 +198,17 @@ test_info_zip_ux(void)
  * Zip entries that use length-at-end.
  */
 static void
-test_extract_length_at_end(void)
+verify_extract_length_at_end(struct archive *a, int check_mode)
 {
-	const char *refname = "test_read_format_zip_length_at_end.zip";
 	struct archive_entry *ae;
-	struct archive *a;
-
-	extract_reference_file(refname);
-	assert((a = archive_read_new()) != NULL);
-	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
-	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
-	assertEqualIntA(a, ARCHIVE_OK, archive_read_open_filename(a, refname, 10240));
 
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
 
 	assertEqualString("hello.txt", archive_entry_pathname(ae));
 	assert(!archive_entry_size_is_set(ae));
 	assertEqualInt(0, archive_entry_size(ae));
+	if (check_mode)
+		assertEqualInt(AE_IFREG | 0644, archive_entry_mode(ae));
 
 	if (libz_enabled) {
 		assertEqualIntA(a, ARCHIVE_OK, archive_read_extract(a, ae, 0));
@@ -182,6 +222,32 @@ test_extract_length_at_end(void)
 
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_free(a));
+}
+
+static void
+test_extract_length_at_end(void)
+{
+	const char *refname = "test_read_format_zip_length_at_end.zip";
+	char *p;
+	size_t s;
+	struct archive *a;
+
+	extract_reference_file(refname);
+
+	/* Verify extraction with seeking reader. */
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_open_filename(a, refname, 10240));
+	verify_extract_length_at_end(a, 1);
+
+	/* Verify extraction with streaming reader. */
+	p = slurpfile(&s, refname);
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, read_open_memory(a, p, s, 108));
+	verify_extract_length_at_end(a, 0);
 }
 
 DEFINE_TEST(test_read_format_zip)
