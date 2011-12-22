5e9ba75c280b4a248fe8c9882b8840e89b8da60f
Tim Kientzle <kientzle@gmail.com>
2011-12-22 01:18:29 -0500

    Basic test and support for symlinks in Zip archives.
    SVN-Revision: 3957

--- libarchive/test/test_read_format_zip.c
+++ libarchive/test/test_read_format_zip.c
@@ -258,9 +258,42 @@ test_extract_length_at_end(void)
 	verify_extract_length_at_end(a, 0);
 }
 
+static void
+test_symlink(void)
+{
+	const char *refname = "test_read_format_zip_symlink.zip";
+	char *p;
+	size_t s;
+	struct archive *a;
+	struct archive_entry *ae;
+
+	extract_reference_file(refname);
+	p = slurpfile(&s, refname);
+
+	/* Symlinks can only be extracted with the seeking reader. */
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_zip(a));
+	assertEqualIntA(a, ARCHIVE_OK, read_open_memory_seek(a, p, s, 1));
+
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualString("file", archive_entry_pathname(ae));
+	assertEqualInt(AE_IFREG, archive_entry_filetype(ae));
+
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualString("symlink", archive_entry_pathname(ae));
+	assertEqualInt(AE_IFLNK, archive_entry_filetype(ae));
+	assertEqualInt(0, archive_entry_size(ae));
+	assertEqualString("file", archive_entry_symlink(ae));
+
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_free(a));
+}
+
 DEFINE_TEST(test_read_format_zip)
 {
 	test_basic();
 	test_info_zip_ux();
 	test_extract_length_at_end();
+	test_symlink();
 }
