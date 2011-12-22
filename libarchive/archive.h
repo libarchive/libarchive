fe9b86254d66411b6bf267a02236d4672258b61b
Tim Kientzle <kientzle@gmail.com>
2011-12-22 00:46:08 -0500

    Remove a few stray references to ARCHIVE_API_VERSION, which was removed some time ago. Also remove the last references to archive_read_data_into_buffer (a mis-designed interface that was deprecated a very long time ago).
    SVN-Revision: 3954

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -125,17 +125,6 @@ extern "C" {
  * version number is printf("%d%03d%03d",a,b,c).  For example, if you
  * know your application requires version 2.12.108 or later, you can
  * assert that ARCHIVE_VERSION >= 2012108.
- *
- * This single-number format was introduced with libarchive 1.9.0 in
- * the libarchive 1.x family and libarchive 2.2.4 in the libarchive
- * 2.x family.  The following may be useful if you really want to do
- * feature detection for earlier libarchive versions (which defined
- * ARCHIVE_API_VERSION and ARCHIVE_API_FEATURE instead):
- *
- * #ifndef ARCHIVE_VERSION_NUMBER
- * #define ARCHIVE_VERSION_NUMBER	\
- *             (ARCHIVE_API_VERSION * 1000000 + ARCHIVE_API_FEATURE * 1000)
- * #endif
  */
 /* Note: Compiler will complain if this does not match archive_entry.h! */
 #define	ARCHIVE_VERSION_NUMBER 3000001
@@ -447,8 +436,6 @@ __LA_DECL int archive_read_data_block(struct archive *a,
  *  'into_fd': writes data to specified filedes
  */
 __LA_DECL int archive_read_data_skip(struct archive *);
-__LA_DECL int archive_read_data_into_buffer(struct archive *,
-			    void *buffer, __LA_SSIZE_T len);
 __LA_DECL int archive_read_data_into_fd(struct archive *, int fd);
 
 /*
