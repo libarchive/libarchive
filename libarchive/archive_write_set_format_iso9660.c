f3804924984fa44364be92bf3cff59cd2be2f24d
Andres Mejia <amejia004@gmail.com>
2011-12-24 16:37:25 -0500

    Fix spelling error.
    SVN-Revision: 3993

--- libarchive/archive_write_set_format_iso9660.c
+++ libarchive/archive_write_set_format_iso9660.c
@@ -6272,7 +6272,7 @@ isoent_gen_joliet_identifier(struct archive_write *a, struct isoent *isoent,
 		if (parent_len + np->mb_len > 240) {
 			archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
 			    "The regulation of Joliet extensions;"
-			    " A lenght of a full-pathname of `%s' is "
+			    " A length of a full-pathname of `%s' is "
 			    "longer than 240 bytes, (p=%d, b=%d)",
 			    archive_entry_pathname(np->file->entry),
 			    (int)parent_len, (int)np->mb_len);
