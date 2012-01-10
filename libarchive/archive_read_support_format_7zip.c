834380d49276dd2b23faaad6b0ba278d79486124
Tim Kientzle <kientzle@gmail.com>
2012-01-10 02:05:12 -0500

    Merge from trunk: always-true comparison.
    SVN-Revision: 4115

--- libarchive/archive_read_support_format_7zip.c
+++ libarchive/archive_read_support_format_7zip.c
@@ -1382,7 +1382,7 @@ decompress(struct archive_read *a, struct _7zip *zip,
 		uint64_t flush_bytes;
 
 		if (!zip->ppmd7_valid || zip->ppmd7_stat < 0 ||
-		    t_avail_in < 0 || t_avail_out <= 0) {
+		    t_avail_out <= 0) {
 			archive_set_error(&(a->archive),
 			    ARCHIVE_ERRNO_MISC,
 			    "Decompression internal error");
