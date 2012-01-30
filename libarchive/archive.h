e74a1a0e7d36cf831c871ac5f46debaf84148a3d
Tim Kientzle <kientzle@gmail.com>
2012-01-29 21:41:45 -0500

    Set trunk version to 3.0.200a for pre-3.1 development. (Mac OS doesn't like 900 here.)
    SVN-Revision: 4215

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -118,13 +118,13 @@ extern "C" {
  * assert that ARCHIVE_VERSION_NUMBER >= 2012108.
  */
 /* Note: Compiler will complain if this does not match archive_entry.h! */
-#define	ARCHIVE_VERSION_NUMBER 3000900
+#define	ARCHIVE_VERSION_NUMBER 3000200
 __LA_DECL int		archive_version_number(void);
 
 /*
  * Textual name/version of the library, useful for version displays.
  */
-#define	ARCHIVE_VERSION_STRING "libarchive 3.0.900a"
+#define	ARCHIVE_VERSION_STRING "libarchive 3.0.200a"
 __LA_DECL const char *	archive_version_string(void);
 
 /* Declare our basic types. */
