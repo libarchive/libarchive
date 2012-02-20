5c64e48d872b1d2c2ad52f1c6cd7748371fcbf09
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2012-02-20 15:31:20 +0900

    Correct console I/O on Windows. Use CONIN$ and CONOUT$ instead of /dev/tty.

--- cpio/cpio.c
+++ cpio/cpio.c
@@ -1139,12 +1139,24 @@ cpio_rename(const char *name)
 	static char buff[1024];
 	FILE *t;
 	char *p, *ret;
+#if defined(_WIN32) && !defined(__CYGWIN__)
+	FILE *to;
 
+	t = fopen("CONIN$", "r");
+	if (t == NULL)
+		return (name);
+	to = fopen("CONOUT$", "w");
+	if (to == NULL)
+		return (name);
+	fprintf(to, "%s (Enter/./(new name))? ", name);
+	fclose(to);
+#else
 	t = fopen("/dev/tty", "r+");
 	if (t == NULL)
 		return (name);
 	fprintf(t, "%s (Enter/./(new name))? ", name);
 	fflush(t);
+#endif
 
 	p = fgets(buff, sizeof(buff), t);
 	fclose(t);
