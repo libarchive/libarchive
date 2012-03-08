de8aa82e8ed105271506ad9d92f16579843ef0cb
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2012-03-08 13:13:53 +0900

    Properly reuse rar->filename.

--- libarchive/archive_read_support_format_rar.c
+++ libarchive/archive_read_support_format_rar.c
@@ -1162,12 +1162,16 @@ read_header(struct archive_read *a, struct archive_entry *entry,
     return (ARCHIVE_FATAL);
   }
   if (rar->filename_allocated < filename_size * 2 + 2) {
-    rar->filename = realloc(rar->filename, filename_size * 2 + 2);
-    if (rar->filename == NULL) {
+    char *newptr;
+    size_t newsize = filename_size * 2 + 2;
+    newptr = realloc(rar->filename, newsize);
+    if (newptr == NULL) {
       archive_set_error(&a->archive, ENOMEM,
                         "Couldn't allocate memory.");
       return (ARCHIVE_FATAL);
     }
+    rar->filename = newptr;
+    rar->filename_allocated = newsize;
   }
   filename = rar->filename;
   memcpy(filename, p, filename_size);
