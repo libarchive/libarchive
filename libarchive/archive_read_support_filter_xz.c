c14e4656685bcaf194ee5cb1357e28f2b1b6a024
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2012-10-14 13:42:32 +0900

    Fix an external program name for lzip decompression.

--- libarchive/archive_read_support_filter_xz.c
+++ libarchive/archive_read_support_filter_xz.c
@@ -972,7 +972,7 @@ lzip_bidder_init(struct archive_read_filter *self)
 {
 	int r;
 
-	r = __archive_read_program(self, "unlzip");
+	r = __archive_read_program(self, "lunzip");
 	/* Note: We set the format here even if __archive_read_program()
 	 * above fails.  We do, after all, know what the format is
 	 * even if we weren't able to read it. */
