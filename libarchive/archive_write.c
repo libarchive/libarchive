6ecfd66c4043ee497b5086f552d1e3961d58300a
Tim Kientzle <kientzle@acm.org>
2013-02-16 18:40:30 -0800

    style fix

--- libarchive/archive_write.c
+++ libarchive/archive_write.c
@@ -505,7 +505,7 @@ _archive_write_close(struct archive *_a)
 
 	/* Finish the last entry if a finish callback is specified */
 	if (a->archive.state == ARCHIVE_STATE_DATA
-		&& a->format_finish_entry != NULL)
+	    && a->format_finish_entry != NULL)
 		r = ((a->format_finish_entry)(a));
 
 	/* Finish off the archive. */
@@ -660,7 +660,7 @@ _archive_write_finish_entry(struct archive *_a)
 	    ARCHIVE_STATE_HEADER | ARCHIVE_STATE_DATA,
 	    "archive_write_finish_entry");
 	if (a->archive.state & ARCHIVE_STATE_DATA
-		&& a->format_finish_entry != NULL)
+	    && a->format_finish_entry != NULL)
 		ret = (a->format_finish_entry)(a);
 	a->archive.state = ARCHIVE_STATE_HEADER;
 	return (ret);
