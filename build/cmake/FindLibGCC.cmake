3a6166558c9a8986f651792e07f46949d3714263
Hans Johnson <hans-johnson@uiowa.edu>
2013-05-16 11:33:29 -0500

    STYLE: Convert CMake-language commands to lower case
    Ancient CMake versions required upper-case commands.  Later
    command names became case-insensitive.  Now the preferred style
    is lower-case.
    This also changes all the key words to lower case. The
    primary reason for changing key words is that all
    documentation for CMakeLists.txt now shows the key words as
    lower case. Even the printed “Mastering CMake v5” uses lower
    case.

--- build/cmake/FindLibGCC.cmake
+++ build/cmake/FindLibGCC.cmake
@@ -4,19 +4,19 @@
 #  LIBGCC_LIBRARIES      - List of libraries when using libgcc
 #  LIBGCC_FOUND          - True if libgcc found.
 
-IF (LIBGCC_LIBRARY)
+if (LIBGCC_LIBRARY)
   # Already in cache, be silent
-  SET(LIBGCC_FIND_QUIETLY TRUE)
-ENDIF (LIBGCC_LIBRARY)
+  set(LIBGCC_FIND_QUIETLY TRUE)
+endif (LIBGCC_LIBRARY)
 
-FIND_LIBRARY(LIBGCC_LIBRARY NAMES gcc libgcc)
+find_library(LIBGCC_LIBRARY NAMES gcc libgcc)
 
 # handle the QUIETLY and REQUIRED arguments and set LIBGCC_FOUND to TRUE if 
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBGCC DEFAULT_MSG LIBGCC_LIBRARY)
 
-IF(LIBGCC_FOUND)
-  SET(LIBGCC_LIBRARIES ${LIBGCC_LIBRARY})
-  SET(HAVE_LIBGCC 1)
-ENDIF(LIBGCC_FOUND)
+if(LIBGCC_FOUND)
+  set(LIBGCC_LIBRARIES ${LIBGCC_LIBRARY})
+  set(HAVE_LIBGCC 1)
+endif(LIBGCC_FOUND)
--- build/cmake/FindNettle.cmake
+++ build/cmake/FindNettle.cmake
@@ -5,19 +5,19 @@
 #  NETTLE_LIBRARIES      - List of libraries when using libnettle.
 #  NETTLE_FOUND          - True if libnettle found.
 
-IF (NETTLE_INCLUDE_DIR)
+if (NETTLE_INCLUDE_DIR)
   # Already in cache, be silent
-  SET(NETTLE_FIND_QUIETLY TRUE)
-ENDIF (NETTLE_INCLUDE_DIR)
+  set(NETTLE_FIND_QUIETLY TRUE)
+endif (NETTLE_INCLUDE_DIR)
 
-FIND_PATH(NETTLE_INCLUDE_DIR nettle/md5.h nettle/ripemd160.h nettle/sha.h)
-FIND_LIBRARY(NETTLE_LIBRARY NAMES nettle libnettle)
+find_path(NETTLE_INCLUDE_DIR nettle/md5.h nettle/ripemd160.h nettle/sha.h)
+find_library(NETTLE_LIBRARY NAMES nettle libnettle)
 
 # handle the QUIETLY and REQUIRED arguments and set NETTLE_FOUND to TRUE if 
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(NETTLE DEFAULT_MSG NETTLE_LIBRARY NETTLE_INCLUDE_DIR)
 
-IF(NETTLE_FOUND)
-  SET(NETTLE_LIBRARIES ${NETTLE_LIBRARY})
-ENDIF(NETTLE_FOUND)
+if(NETTLE_FOUND)
+  set(NETTLE_LIBRARIES ${NETTLE_LIBRARY})
+endif(NETTLE_FOUND)
