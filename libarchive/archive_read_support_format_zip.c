ecc435306e45a8bae9515fe2c5bb33600cc5a192
Paul Barker <paul@paulbarker.me.uk>
2014-04-06 23:12:26 +0100

    Use snprintf to print to zip->format_name
    zip->format_name is a fixed size character array so ensure that the number of
    characters written to the array is appropriately limited.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- libarchive/archive_read_support_format_zip.c
+++ libarchive/archive_read_support_format_zip.c
@@ -744,7 +744,7 @@ zip_read_local_file_header(struct archive_read *a, struct archive_entry *entry,
 		zip->end_of_entry = 1;
 
 	/* Set up a more descriptive format name. */
-	sprintf(zip->format_name, "ZIP %d.%d (%s)",
+	snprintf(zip->format_name, sizeof(zip->format_name), "ZIP %d.%d (%s)",
 	    version / 10, version % 10,
 	    compression_name(zip->entry->compression));
 	a->archive.archive_format_name = zip->format_name;
