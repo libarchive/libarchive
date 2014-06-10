75b78d5c1300c01da6c8fcaf6b9ede6d00a6e5d0
Sebastian Freundt <freundt@ga-group.nl>
2014-06-10 12:28:18 +0000

    Hygiene, always use xmemmem() because memmem() is a GNU extension

--- libarchive/archive_read_support_format_warc.c
+++ libarchive/archive_read_support_format_warc.c
@@ -720,7 +720,7 @@ static const char*
 _warc_find_eoh(const char *buf, size_t bsz)
 {
 	static const char _marker[] = "\r\n\r\n";
-	const char *hit = memmem(buf, bsz, _marker, sizeof(_marker) - 1U);
+	const char *hit = xmemmem(buf, bsz, _marker, sizeof(_marker) - 1U);
 
 	if (hit != NULL) {
 		hit += sizeof(_marker) - 1U;
