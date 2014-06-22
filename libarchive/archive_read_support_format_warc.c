5d0e11c8fd1e0608f537ab38f6209510744f05fd
Tim Kientzle <kientzle@acm.org>
2014-06-22 10:50:17 -0700

    Reformat long calculation, make consistent across copies

--- libarchive/archive_read_support_format_warc.c
+++ libarchive/archive_read_support_format_warc.c
@@ -509,10 +509,13 @@ time_from_tm(struct tm *t)
         if (mktime(t) == (time_t)-1)
                 return ((time_t)-1);
         /* Then we can compute timegm() from first principles. */
-        return (t->tm_sec + t->tm_min * 60 + t->tm_hour * 3600
-            + t->tm_yday * 86400 + (t->tm_year - 70) * 31536000
-            + ((t->tm_year - 69) / 4) * 86400 -
-            ((t->tm_year - 1) / 100) * 86400
+        return (t->tm_sec
+            + t->tm_min * 60
+            + t->tm_hour * 3600
+            + t->tm_yday * 86400
+            + (t->tm_year - 70) * 31536000
+            + ((t->tm_year - 69) / 4) * 86400
+            - ((t->tm_year - 1) / 100) * 86400
             + ((t->tm_year + 299) / 400) * 86400);
 #endif
 }
--- libarchive/archive_read_support_format_xar.c
+++ libarchive/archive_read_support_format_xar.c
@@ -1107,20 +1107,23 @@ static time_t
 time_from_tm(struct tm *t)
 {
 #if HAVE_TIMEGM
-	/* Use platform timegm() if available. */
-	return (timegm(t));
+        /* Use platform timegm() if available. */
+        return (timegm(t));
 #elif HAVE__MKGMTIME64
-	return (_mkgmtime64(t));
+        return (_mkgmtime64(t));
 #else
-	/* Else use direct calculation using POSIX assumptions. */
-	/* First, fix up tm_yday based on the year/month/day. */
-	mktime(t);
-	/* Then we can compute timegm() from first principles. */
-	return (t->tm_sec + t->tm_min * 60 + t->tm_hour * 3600
-	    + t->tm_yday * 86400 + (t->tm_year - 70) * 31536000
-	    + ((t->tm_year - 69) / 4) * 86400 -
-	    ((t->tm_year - 1) / 100) * 86400
-	    + ((t->tm_year + 299) / 400) * 86400);
+        /* Else use direct calculation using POSIX assumptions. */
+        /* First, fix up tm_yday based on the year/month/day. */
+        mktime(t);
+        /* Then we can compute timegm() from first principles. */
+        return (t->tm_sec
+            + t->tm_min * 60
+            + t->tm_hour * 3600
+            + t->tm_yday * 86400
+            + (t->tm_year - 70) * 31536000
+            + ((t->tm_year - 69) / 4) * 86400
+            - ((t->tm_year - 1) / 100) * 86400
+            + ((t->tm_year + 299) / 400) * 86400);
 #endif
 }
 
