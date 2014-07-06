ae086fb2635b8e89a7f0a9ce65b80b8c641576e0
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2014-07-06 12:06:15 +0900

    Plug memory leaks which Clang Static Analyser pointed out.

--- libarchive/archive_util.c
+++ libarchive/archive_util.c
@@ -566,8 +566,11 @@ archive_utility_string_sort_helper(char **strings, unsigned int n)
     {
       lesser_count++;
       tmp = (char **)realloc(lesser, lesser_count * sizeof(char *));
-      if (!tmp)
+      if (!tmp) {
+        free(greater);
+        free(lesser);
         return (ARCHIVE_FATAL);
+      }
       lesser = tmp;
       lesser[lesser_count - 1] = strings[i];
     }
@@ -575,8 +578,11 @@ archive_utility_string_sort_helper(char **strings, unsigned int n)
     {
       greater_count++;
       tmp = (char **)realloc(greater, greater_count * sizeof(char *));
-      if (!tmp)
+      if (!tmp) {
+        free(greater);
+        free(lesser);
         return (ARCHIVE_FATAL);
+      }
       greater = tmp;
       greater[greater_count - 1] = strings[i];
     }
