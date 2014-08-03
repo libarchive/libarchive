7b759a662c5816273691e594d2281686139fad1d
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2014-08-03 15:08:41 +0900

    Properly set a error message when a client seeker callback is not defined.

--- libarchive/archive_read.c
+++ libarchive/archive_read.c
@@ -230,8 +230,11 @@ client_seek_proxy(struct archive_read_filter *self, int64_t offset, int whence)
 	 * other libarchive code that assumes a successful forward
 	 * seek means it can also seek backwards.
 	 */
-	if (self->archive->client.seeker == NULL)
+	if (self->archive->client.seeker == NULL) {
+		archive_set_error(&self->archive->archive, ARCHIVE_ERRNO_MISC,
+		    "Current client reader does not support seeking a device");
 		return (ARCHIVE_FAILED);
+	}
 	return (self->archive->client.seeker)(&self->archive->archive,
 	    self->data, offset, whence);
 }
