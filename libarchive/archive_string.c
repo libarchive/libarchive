bdae59c77256f4325d91a93ea0f95e3e92fdf10f
Nicolas Cornu <ncornu@aldebaran.com>
2014-12-11 21:36:08 +0100

    Add a support for utf-8 in archive_entry fields

--- libarchive/archive_string.c
+++ libarchive/archive_string.c
@@ -4062,6 +4062,19 @@ archive_mstring_copy_wcs(struct archive_mstring *aes, const wchar_t *wcs)
 }
 
 int
+archive_mstring_copy_utf8(struct archive_mstring *aes, const char *utf8)
+{
+  if (utf8 == NULL) {
+    aes->aes_set = 0;
+  }
+  aes->aes_set = AES_SET_UTF8;
+  archive_string_empty(&(aes->aes_mbs));
+  archive_string_empty(&(aes->aes_wcs));
+  archive_strncpy(&(aes->aes_utf8), utf8, strlen(utf8));
+  return strlen(utf8);
+}
+
+int
 archive_mstring_copy_wcs_len(struct archive_mstring *aes, const wchar_t *wcs,
     size_t len)
 {
