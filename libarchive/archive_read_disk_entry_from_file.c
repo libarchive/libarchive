c958ca33a193ba94a3f83ef9f4052a9a78838e10
Bjoern Jacke <bjoern@j3e.de>
2016-01-05 13:12:08 +0100

    use _PC_MIN_HOLE_SIZE for SEEK_HOLE only if available
    SEEK_HOLE/SEEK_DATA is availbale on Linux since kernel 3.1 but
    _PC_MIN_HOLE_SIZE is not supported. So we just do that check if we have that
    define.

--- libarchive/archive_read_disk_entry_from_file.c
+++ libarchive/archive_read_disk_entry_from_file.c
@@ -1125,7 +1125,7 @@ exit_setup_sparse:
 	return (exit_sts);
 }
 
-#elif defined(SEEK_HOLE) && defined(SEEK_DATA) && defined(_PC_MIN_HOLE_SIZE)
+#elif defined(SEEK_HOLE) && defined(SEEK_DATA)
 
 /*
  * FreeBSD and Solaris sparse interface.
@@ -1162,8 +1162,10 @@ setup_sparse(struct archive_read_disk *a,
 	}
 
 	if (*fd >= 0) {
+#ifdef _PC_MIN_HOLE_SIZE
 		if (fpathconf(*fd, _PC_MIN_HOLE_SIZE) <= 0)
 			return (ARCHIVE_OK);
+#endif
 		initial_off = lseek(*fd, 0, SEEK_CUR);
 		if (initial_off != 0)
 			lseek(*fd, 0, SEEK_SET);
@@ -1174,8 +1176,10 @@ setup_sparse(struct archive_read_disk *a,
 		if (path == NULL)
 			path = archive_entry_pathname(entry);
 			
+#ifdef _PC_MIN_HOLE_SIZE
 		if (pathconf(path, _PC_MIN_HOLE_SIZE) <= 0)
 			return (ARCHIVE_OK);
+#endif
 		*fd = open(path, O_RDONLY | O_NONBLOCK | O_CLOEXEC);
 		if (*fd < 0) {
 			archive_set_error(&a->archive, errno,
