e614bd60a2367eda738b435d56f37a9e39c32844
Tim Kientzle <kientzle@acm.org>
2016-02-13 22:06:09 -0800

    Fix stringification here

--- libarchive/archive_util.c
+++ libarchive/archive_util.c
@@ -161,9 +161,11 @@ const char *
 archive_liblz4_version(void)
 {
 #if defined(HAVE_LZ4_H) && defined(HAVE_LIBLZ4)
-#define NUMBER(x) #x
+#define str(s) #s
+#define NUMBER(x) str(x)
 	return NUMBER(LZ4_VERSION_MAJOR) "." NUMBER(LZ4_VERSION_MINOR) "." NUMBER(LZ4_VERSION_RELEASE);
 #undef NUMBER
+#undef str
 #else
 	return NULL;
 #endif
