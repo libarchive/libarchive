fbfe30450e6b224f669348c8506033284625536d
Tim Kientzle <kientzle@acm.org>
2016-04-24 20:35:45 -0700

    Accomodate different return codes on Windows

--- cat/test/test_error.c
+++ cat/test/test_error.c
@@ -29,7 +29,7 @@ DEFINE_TEST(test_error)
 	const char *reffile = "test_expand.error";
 
 	assertFileNotExists(reffile);
-	assertEqualInt(256, systemf("%s %s >test.out 2>test.err", testprog, reffile));
+	assert(0 != systemf("%s %s >test.out 2>test.err", testprog, reffile));
 
 	assertEmptyFile("test.out");
 	assertNonEmptyFile("test.err");
