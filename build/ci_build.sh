944b8aab7c9cf4417a6d2384b37ad35d30309a2d
Martin Matuska <martin@matuska.org>
2016-11-07 15:52:36 +0100

    ci_build: be more verbose on test errors if building with autotools

--- build/ci_build.sh
+++ build/ci_build.sh
@@ -89,8 +89,15 @@ for action in ${ACTIONS}; do
 		;;
 		test)
 			case "${BUILD_SYSTEM}" in
-				autotools) make ${MAKE_ARGS} check ;;
-				cmake) make ${MAKE_ARGS} test ;;
+				autotools)
+					if ! make ${MAKE_ARGS} check; then
+						RET="$?"
+						cat test-suite.log
+					fi
+					;;
+				cmake)
+					make ${MAKE_ARGS} test
+					;;
 			esac
 			RET="$?"
 		;;
