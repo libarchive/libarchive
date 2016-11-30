b1d69c971406cb33ddaae741b98f2bef4cfc3222
Graham Percival <gperciva@tarsnap.com>
2016-11-30 12:07:58 -0800

    Spelling: uncapable -> incapable
    Sponsored by:	Tarsnap Backup Inc.

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -562,7 +562,7 @@ __LA_DECL la_int64_t		 archive_read_header_position(struct archive *);
  * we cannot say whether there are encrypted entries, then
  * ARCHIVE_READ_FORMAT_ENCRYPTION_DONT_KNOW is returned.
  * In general, this function will return values below zero when the
- * reader is uncertain or totally uncapable of encryption support.
+ * reader is uncertain or totally incapable of encryption support.
  * When this function returns 0 you can be sure that the reader
  * supports encryption detection but no encrypted entries have
  * been found yet.
