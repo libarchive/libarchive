f167e8d9d521e5536b37098501c6396ab65452e6
Martin Matuska <martin@matuska.org>
2016-12-08 02:45:41 +0100

    Do not read beyond strtab table size in _ar_read_header()
    Reported-by:	OSS-Fuzz issue 237

--- libarchive/archive_read_support_format_ar.c
+++ libarchive/archive_read_support_format_ar.c
@@ -315,7 +315,7 @@ _ar_read_header(struct archive_read *a, struct archive_entry *entry,
 		 * If we can't look up the real name, warn and return
 		 * the entry with the wrong name.
 		 */
-		if (ar->strtab == NULL || number > ar->strtab_size) {
+		if (ar->strtab == NULL || number >= ar->strtab_size) {
 			archive_set_error(&a->archive, EINVAL,
 			    "Can't find long filename for GNU/SVR4 archive entry");
 			archive_entry_copy_pathname(entry, filename);
