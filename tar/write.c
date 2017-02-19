cdd5f101880adf5c78b5e126164d3af04f6a9263
Tim Kientzle <kientzle@acm.org>
2017-02-18 19:55:21 -0800

    Correctly report 0 compression when nothing has yet been written.

--- tar/write.c
+++ tar/write.c
@@ -1010,7 +1010,7 @@ report_write(struct bsdtar *bsdtar, struct archive *a,
 	uncomp = archive_filter_bytes(a, 0);
 	fprintf(stderr, "In: %d files, %s bytes;",
 	    archive_file_count(a), tar_i64toa(uncomp));
-	if (comp > uncomp)
+	if (comp >= uncomp)
 		compression = 0;
 	else
 		compression = (int)((uncomp - comp) * 100 / uncomp);
