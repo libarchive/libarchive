ee479f802046f7f3fd8dd1f3f4a58c94bb6b2e2a
Joerg Sonnenberger <joerg@bec.de>
2017-04-29 15:50:24 +0200

    Redo 1e660621b4b00eaf4ef7a508aedf52c5a421bd2e, only skip the memmove.

--- libarchive/archive_string.c
+++ libarchive/archive_string.c
@@ -200,11 +200,10 @@ static int archive_string_append_unicode(struct archive_string *,
 static struct archive_string *
 archive_string_append(struct archive_string *as, const char *p, size_t s)
 {
-	if (s == 0)
-		return (as);
 	if (archive_string_ensure(as, as->length + s + 1) == NULL)
 		return (NULL);
-	memmove(as->s + as->length, p, s);
+	if (s)
+		memmove(as->s + as->length, p, s);
 	as->length += s;
 	as->s[as->length] = 0;
 	return (as);
