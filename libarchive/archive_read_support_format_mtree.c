8b23b12978b0e60d7449a3c5946b75ac9081e9c4
Joerg Sonnenberger <joerg@bec.de>
2017-04-29 22:15:52 +0200

    Tighten mode check: '8' and '9' don't make good octal digits.

--- libarchive/archive_read_support_format_mtree.c
+++ libarchive/archive_read_support_format_mtree.c
@@ -1535,14 +1535,14 @@ parse_keyword(struct archive_read *a, struct mtree *mtree,
 		if (strcmp(key, "md5") == 0 || strcmp(key, "md5digest") == 0)
 			break;
 		if (strcmp(key, "mode") == 0) {
-			if (val[0] >= '0' && val[0] <= '9') {
+			if (val[0] >= '0' && val[0] <= '7') {
 				*parsed_kws |= MTREE_HAS_PERM;
 				archive_entry_set_perm(entry,
 				    (mode_t)mtree_atol8(&val));
 			} else {
 				archive_set_error(&a->archive,
 				    ARCHIVE_ERRNO_FILE_FORMAT,
-				    "Symbolic mode \"%s\" unsupported", val);
+				    "Symbolic or non-octal mode \"%s\" unsupported", val);
 				return ARCHIVE_WARN;
 			}
 			break;
