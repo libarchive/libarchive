5eac2a780fbfaac73a9252f0c1b83785b8e880ea
Jan Osusky <Jan.Osusky@iblsoft.com>
2018-03-20 16:25:15 +0100

    Make mtree less eager to bid
    At least one keyword is required to consider a file an mtree archive.
    Moreover the search will stop if no keyword is found in the first 6 lines.
    This prevents misidentification of a simple CSV files as mtree archives.
    CSV file shall be identified as "raw" - test has been added.
    The original use-case was processing of gzipped CSV files with libarchive.
    The decompression went fine but then the mtree format bid for it ... and failed.

--- libarchive/test/test_read_format_raw.c
+++ libarchive/test/test_read_format_raw.c
@@ -36,6 +36,7 @@ DEFINE_TEST(test_read_format_raw)
 	const char *reffile1 = "test_read_format_raw.data";
 	const char *reffile2 = "test_read_format_raw.data.Z";
 	const char *reffile3 = "test_read_format_raw.bufr";
+	const char *reffile4 = "test_read_format_raw.csv";
 
 	/* First, try pulling data out of an uninterpretable file. */
 	extract_reference_file(reffile1);
@@ -101,6 +102,31 @@ DEFINE_TEST(test_read_format_raw)
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
 	assertEqualIntA(a, ARCHIVE_OK,
 	    archive_read_open_filename(a, reffile3, 1));
+
+	/* First (and only!) Entry */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualString("data", archive_entry_pathname(ae));
+	assertEqualInt(archive_entry_is_encrypted(ae), 0);
+	assertEqualIntA(a, archive_read_has_encrypted_entries(a), ARCHIVE_READ_FORMAT_ENCRYPTION_UNSUPPORTED);
+	/* Most fields should be unset (unknown) */
+	assert(!archive_entry_size_is_set(ae));
+	assert(!archive_entry_atime_is_set(ae));
+	assert(!archive_entry_ctime_is_set(ae));
+	assert(!archive_entry_mtime_is_set(ae));
+
+	/* Test EOF */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
+	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
+
+	/* Forth, try with file which fooled us in past - a CSV appeared to be mtree. */
+	extract_reference_file(reffile4);
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_raw(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	assertEqualIntA(a, ARCHIVE_OK,
+	    archive_read_open_filename(a, reffile4, 1));
 
 	/* First (and only!) Entry */
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
