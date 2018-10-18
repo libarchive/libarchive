bf234723494dfd847bf7b030f2227c7bbd99b36b
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2018-10-18 16:02:47 +0100

    Don't #undef stat and #define stat(). stat is one of those unfortunate identifiers for which there is both a struct and a function. MinGW uses a #define for setting struct stat to be the right struct, so doing #undef stat to clear the way for a #define for stat() the function inadvertantly clobbers the selected stat struct. To avoid this, we define la_stat() to _la_stat on Windows and stat() elsewhere, and then use la_stat() instead of stat().

--- libarchive/archive_read_disk_posix.c
+++ libarchive/archive_read_disk_posix.c
@@ -2450,7 +2450,7 @@ tree_current_stat(struct tree *t)
 #else
 		if (tree_enter_working_dir(t) != 0)
 			return NULL;
-		if (stat(tree_current_access_path(t), &t->st) != 0)
+		if (la_stat(tree_current_access_path(t), &t->st) != 0)
 #endif
 			return NULL;
 		t->flags |= hasStat;
