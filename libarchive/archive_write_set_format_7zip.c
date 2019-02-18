e5502783ec70f93ba3536adec73115d14efd441e
Patrick Cheng <patrickyccheng@gmail.com>
2019-02-17 20:34:01 -0800

    Style changes removed

--- libarchive/archive_write_set_format_7zip.c
+++ libarchive/archive_write_set_format_7zip.c
@@ -819,7 +819,7 @@ _7z_close(struct archive_write *a)
 		header_offset = header_size = 0;
 		header_crc32 = 0;
 	}
-
+	
 	length = zip->temp_offset;
 
 	/*
@@ -1474,7 +1474,7 @@ file_cmp_node(const struct archive_rb_node *n1,
 		return (memcmp(f1->utf16name, f2->utf16name, f1->name_len));
 	return (f1->name_len > f2->name_len)?1:-1;
 }
-
+        
 static int
 file_cmp_key(const struct archive_rb_node *n, const void *key)
 {
