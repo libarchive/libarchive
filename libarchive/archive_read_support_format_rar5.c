4bd12b6f40cb4413c8970c52f15f3b6885b5e32f
Grzegorz Antoniak <ga@anadoxin.org>
2019-05-01 06:21:58 +0200

    RAR5 reader: fix buffer overflow when parsing huffman tables.
    RAR5 compresses its Huffman tables by using an algorithm similar to Run
    Length Encoding. During uncompression of those tables, RAR5 reader
    didn't perform enough checks to prevent from buffer overflow in some
    cases.
    
    This commit adds additional check that prevents from encountering a
    buffer overflow error in some files.
    
    The commit also adds a unit test to guard against regression of this
    issue.
    
    Fixes OSSFuzz case #12466.

--- libarchive/archive_read_support_format_rar5.c
+++ libarchive/archive_read_support_format_rar5.c
@@ -2323,7 +2323,7 @@ static int parse_tables(struct archive_read* a, struct rar5* rar,
                 int k;
 
                 /* Fill zeroes. */
-                for(k = 0; k < value + 2; k++) {
+                for(k = 0; (k < value + 2) && (w < HUFF_BC); k++) {
                     bit_length[w++] = 0;
                 }
             }
--- libarchive/test/test_read_format_rar5.c
+++ libarchive/test/test_read_format_rar5.c
@@ -950,3 +950,18 @@ DEFINE_TEST(test_read_format_rar5_extra_field_version)
 
     EPILOGUE();
 }
+
+DEFINE_TEST(test_read_format_rar5_readtables_overflow)
+{
+    uint8_t buf[16];
+
+    PROLOGUE("test_read_format_rar5_readtables_overflow.rar");
+
+    assertA(0 == archive_read_next_header(a, &ae));
+    /* This archive is invalid. However, processing it shouldn't cause any
+     * buffer overflow errors during reading rar5 tables. */
+    assertA(0 == archive_read_data(a, buf, sizeof(buf)));
+    assertA(ARCHIVE_EOF == archive_read_next_header(a, &ae));
+
+    EPILOGUE();
+}
