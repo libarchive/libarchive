libarchive/libarchive

commit d91ef58185a2f7fa469a0391a6787b60e5b767c9

Feed at most UINT_MAX data to gzip.
Fixes #1169

libarchive/archive_read_support_filter_gzip.c
@@ -37,6 +37,9 @@ __FBSDID("$FreeBSD$");
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
+#ifdef HAVE_LIMITS_H
+#include <limits.h>
+#endif
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -440,6 +443,8 @@ gzip_filter_read(struct archive_read_filter *self, const void **p)
 			    "truncated gzip input");
 			return (ARCHIVE_FATAL);
 		}
+		if (avail_in > UINT_MAX)
+			avail_in = UINT_MAX;
 		state->stream.avail_in = (uInt)avail_in;
 
 		/* Decompress and consume some of that data. */

commit cb45ca8d812ad010c8d0a9853ea7ebae362a0e86

fix build with -Werror=sign-compare
Fixes: d91ef58185a2f7fa469a0391a6787b60e5b767c9

libarchive/archive_read_support_filter_gzip.c
@@ -443,7 +443,7 @@ gzip_filter_read(struct archive_read_filter *self, const void **p)
 			    "truncated gzip input");
 			return (ARCHIVE_FATAL);
 		}
-		if (avail_in > UINT_MAX)
+		if (avail_in > (ssize_t)UINT_MAX)
 			avail_in = UINT_MAX;
 		state->stream.avail_in = (uInt)avail_in;
 