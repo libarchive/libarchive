328196a29c5091d2358a5efd2a59ab029c2165c5
Martin Matuska <martin@matuska.org>
2020-01-26 00:35:50 +0100

    PAX writer: fix entry uname or gname longer than 32 characters
    Fixes #1319

--- libarchive/archive_write_set_format_ustar.c
+++ libarchive/archive_write_set_format_ustar.c
@@ -513,9 +513,11 @@ __archive_write_format_header_ustar(struct archive_write *a, char h[512],
 	}
 	if (copy_length > 0) {
 		if (copy_length > USTAR_uname_size) {
-			archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
-			    "Username too long");
-			ret = ARCHIVE_FAILED;
+			if (tartype != 'x') {
+				archive_set_error(&a->archive,
+				    ARCHIVE_ERRNO_MISC, "Username too long");
+				ret = ARCHIVE_FAILED;
+			}
 			copy_length = USTAR_uname_size;
 		}
 		memcpy(h + USTAR_uname_offset, p, copy_length);
@@ -536,9 +538,11 @@ __archive_write_format_header_ustar(struct archive_write *a, char h[512],
 	}
 	if (copy_length > 0) {
 		if (strlen(p) > USTAR_gname_size) {
-			archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
-			    "Group name too long");
-			ret = ARCHIVE_FAILED;
+			if (tartype != 'x') {
+				archive_set_error(&a->archive,
+				    ARCHIVE_ERRNO_MISC, "Group name too long");
+				ret = ARCHIVE_FAILED;
+			}
 			copy_length = USTAR_gname_size;
 		}
 		memcpy(h + USTAR_gname_offset, p, copy_length);
