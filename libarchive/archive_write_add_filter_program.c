2c3ae11cee52dbf2bcb83528d06add7410b7494d
Emil Velikov <emil.l.velikov@gmail.com>
2020-04-25 15:46:41 +0100

    Remove CloseHandle() from __archive_write_program_free
    The call to CloseHandle() is asymmetric and wrong.
    
    The handle is created during the __achive_create_child() call in
    __archive_write_program_open() aka the filter open callback.
    
    While CloseHandle() is already called in the filter close callback aka
    __archive_write_program_close().
    
    Remove the incorrect call to CloseHandle() from the bidder free callback
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- libarchive/archive_write_add_filter_program.c
+++ libarchive/archive_write_add_filter_program.c
@@ -196,10 +196,6 @@ __archive_write_program_free(struct archive_write_program_data *data)
 {
 
 	if (data) {
-#if defined(_WIN32) && !defined(__CYGWIN__)
-		if (data->child)
-			CloseHandle(data->child);
-#endif
 		free(data->program_name);
 		free(data->child_buf);
 		free(data);
