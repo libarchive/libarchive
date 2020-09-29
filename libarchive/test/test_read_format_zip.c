4b8d0554dd7d6510965537cb890dbf5f9ed68a40
Masalskaya, Anna <anna.masalskaya@intel.com>
2020-09-30 00:21:41 +0300

    fix test failure

--- libarchive/test/test_read_format_zip.c
+++ libarchive/test/test_read_format_zip.c
@@ -984,19 +984,17 @@ DEFINE_TEST(test_read_format_zip_7z_lzma)
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_zip(a));
 	assertEqualIntA(a, ARCHIVE_OK,
 		archive_read_open_filename(a, refname, 10240));
-
-	while(1) {
-		int res = archive_read_next_header(a, &ae);
-		if (res == ARCHIVE_EOF)
-			break;
-
-		assertEqualInt(ARCHIVE_OK, res);
-		if (archive_entry_filetype(ae) == AE_IFLNK)
-			assertEqualString("../samples/abc_measurement_analysis_sample"
-				"/src/abc_measurement_analysis_sample.py",
-				archive_entry_symlink(ae));
-	}
-
+	//read directories
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	//read symlink
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualInt(AE_IFLNK, archive_entry_filetype(ae));
+	assertEqualString("../samples/abc_measurement_analysis_sample"
+		"/src/abc_measurement_analysis_sample.py",
+		archive_entry_symlink(ae));
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_free(a));
 }
