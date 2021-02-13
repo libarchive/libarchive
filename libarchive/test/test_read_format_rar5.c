05ebb55896d10a9737dad9ae0303f7f45489ba6f
Grzegorz Antoniak <ga@anadoxin.org>
2021-02-13 09:08:13 +0100

    RAR5 reader: fixed out of bounds read in some files
    Added more range checks in the bit stream reading functions
    (read_bits_16 and read_bits_32) in order to better guard against out of
    memory reads.
    
    This commit contains a test with OSSFuzz sample #30448.

--- libarchive/test/test_read_format_rar5.c
+++ libarchive/test/test_read_format_rar5.c
@@ -1295,3 +1295,19 @@ DEFINE_TEST(test_read_format_rar5_sfx)
 	assertEqualMem(buff, test_txt, size);
 }
 
+DEFINE_TEST(test_read_format_rar5_decode_number_out_of_bounds_read)
+{
+	/* oss fuzz 30448 */
+
+	char buf[4096];
+	PROLOGUE("test_read_format_rar5_decode_number_out_of_bounds_read.rar");
+
+	/* Return codes of those calls are ignored, because this sample file
+	 * is invalid. However, the unpacker shouldn't produce any SIGSEGV
+	 * errors during processing. */
+
+	(void) archive_read_next_header(a, &ae);
+	while(0 < archive_read_data(a, buf, sizeof(buf))) {}
+
+	EPILOGUE();
+}
