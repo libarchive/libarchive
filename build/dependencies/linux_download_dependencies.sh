352ca620fa3e865faf68b0d7c28a1dd9df0fabab
jiat75 <jiat0218@gmail.com>
2021-10-15 21:48:51 +0800

    Added support for downloading dependencies from yum

--- build/dependencies/linux_download_dependencies.sh
+++ build/dependencies/linux_download_dependencies.sh
@@ -26,4 +26,27 @@ then
     done
 
     echo -e "${GREEN}All dependencies successfully installed${NO_COLOR}"
+# Check if yum is installed on machine
+elif command -v yum &> /dev/null
+then
+    echo -e "${GREEN}yum command found. Using apt to install dependencies${NO_COLOR}"
+    # List yum package name for all dependencies
+    declare -a yum_depends=("libb2-devel" "zlib-devel" "bzip2-devel" "lzo-devel"
+                            "lz4-devel" "libzstd-devel" "lzma-dev" "lrzip"
+                            "openssl-dev" "libxml2-devel" "libexpat-devel"
+                            "pcre3-devel" "mbedtls-devel" "nettle-devel")
+
+    yum update
+    yum upgrade
+
+    # Install each listed dependency
+    for i in "${apt_depends[@]}"
+    do
+        yum install "$i"
+    done
+
+    echo -e "${GREEN}All dependencies successfully installed${NO_COLOR}"
+
+else
+    echo -e "${RED}No suitable package manager found. Dependencies were not installed.${NO_COLOR}"
 fi
