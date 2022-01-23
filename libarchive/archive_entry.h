6cc25e7d0dd185287dc13f32274c8ae19822a596
Alexey Pelykh <alexey.pelykh@gmail.com>
2022-01-23 09:40:58 +0100

    Include android_lf.h only for libarchive sources

--- libarchive/archive_entry.h
+++ libarchive/archive_entry.h
@@ -99,7 +99,7 @@ typedef ssize_t la_ssize_t;
 #endif
 
 /* Large file support for Android */
-#ifdef __ANDROID__
+#if defined(__LIBARCHIVE_BUILD) && defined(__ANDROID__)
 #include "android_lf.h"
 #endif
 
