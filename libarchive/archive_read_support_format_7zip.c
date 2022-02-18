e2e5429e4b9cce6447a41444e26772587e88812e
cielavenir <cielartisan@gmail.com>
2022-02-19 00:23:16 +0900

    code review

--- libarchive/archive_read_support_format_7zip.c
+++ libarchive/archive_read_support_format_7zip.c
@@ -988,7 +988,10 @@ ppmd_read(void *p)
 	Byte b;
 
 	if (zip->ppstream.avail_in <= 0) {
-		// Ppmd7_DecodeSymbol might require reading multiple bytes and we are on boundary; last resort to read using __archive_read_ahead.
+		/*
+                 * Ppmd7_DecodeSymbol might require reading multiple bytes and we are on boundary;
+                 * last resort to read using __archive_read_ahead.
+                 */
 		ssize_t bytes_avail = 0;
 		const uint8_t* data = __archive_read_ahead(a, zip->ppstream.stream_in+1, &bytes_avail);
 		if(bytes_avail < zip->ppstream.stream_in+1) {
@@ -999,15 +1002,12 @@ ppmd_read(void *p)
 		}
 		zip->ppstream.next_in++;
 		b = data[zip->ppstream.stream_in];
-		zip->ppstream.avail_in--;
-		zip->ppstream.total_in++;
-		zip->ppstream.stream_in++;
 	} else {
 		b = *zip->ppstream.next_in++;
-		zip->ppstream.avail_in--;
-		zip->ppstream.total_in++;
-		zip->ppstream.stream_in++;
 	}
+	zip->ppstream.avail_in--;
+	zip->ppstream.total_in++;
+	zip->ppstream.stream_in++;
 	return (b);
 }
 
