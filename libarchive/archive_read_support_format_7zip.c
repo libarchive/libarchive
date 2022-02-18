72ce1ff7c6857a7334baa05884e69b9264a2199c
Martin Matuska <martin@matuska.org>
2022-02-19 00:38:53 +0100

    7zip reader: style fixes after 3962d596d

--- libarchive/archive_read_support_format_7zip.c
+++ libarchive/archive_read_support_format_7zip.c
@@ -989,14 +989,17 @@ ppmd_read(void *p)
 
 	if (zip->ppstream.avail_in <= 0) {
 		/*
-                 * Ppmd7_DecodeSymbol might require reading multiple bytes and we are on boundary;
-                 * last resort to read using __archive_read_ahead.
-                 */
+		 * Ppmd7_DecodeSymbol might require reading multiple bytes
+		 * and we are on boundary;
+		 * last resort to read using __archive_read_ahead.
+		 */
 		ssize_t bytes_avail = 0;
-		const uint8_t* data = __archive_read_ahead(a, zip->ppstream.stream_in+1, &bytes_avail);
+		const uint8_t* data = __archive_read_ahead(a,
+		    zip->ppstream.stream_in+1, &bytes_avail);
 		if(bytes_avail < zip->ppstream.stream_in+1) {
-			archive_set_error(&a->archive, ARCHIVE_ERRNO_FILE_FORMAT,
-				"Truncated 7z file data");
+			archive_set_error(&a->archive,
+			    ARCHIVE_ERRNO_FILE_FORMAT,
+			    "Truncated 7z file data");
 			zip->ppstream.overconsumed = 1;
 			return (0);
 		}
