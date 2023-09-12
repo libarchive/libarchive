3bd918d92f8c34ba12de9c6604d96f9e262a59fc
Martin Matuska <martin@matuska.de>
2023-09-12 08:54:47 +0200

    tests: fix zstd long option test for 32-bit architectures
    Fixes #1968

--- libarchive/test/test_write_filter_zstd.c
+++ libarchive/test/test_write_filter_zstd.c
@@ -161,8 +161,12 @@ DEFINE_TEST(test_write_filter_zstd)
 	    archive_write_set_filter_option(a, NULL, "max-frame-size", "1048576"));
 #endif
 #if ZSTD_VERSION_NUMBER >= MINVER_LONG
-	assertEqualIntA(a, ARCHIVE_OK,
-	    archive_write_set_filter_option(a, NULL, "long", "27"));
+	if ((int)(sizeof(size_t) == 4))
+		assertEqualIntA(a, ARCHIVE_OK,
+		    archive_write_set_filter_option(a, NULL, "long", "26"));
+	else
+		assertEqualIntA(a, ARCHIVE_OK,
+		    archive_write_set_filter_option(a, NULL, "long", "27"));
 	assertEqualIntA(a, ARCHIVE_FAILED,
 	    archive_write_set_filter_option(a, NULL, "long", "-1")); /* negative */
 #endif
