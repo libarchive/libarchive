313aa1fa10b657de791e3202c168a6c833bc3543
Martin Matuska <martin@matuska.de>
2024-04-26 11:22:15 +0200

    Release 3.7.4

--- libarchive/archive.h
+++ libarchive/archive.h
@@ -155,7 +155,7 @@ __LA_DECL int		archive_version_number(void);
 /*
  * Textual name/version of the library, useful for version displays.
  */
-#define	ARCHIVE_VERSION_ONLY_STRING "3.7.4dev"
+#define	ARCHIVE_VERSION_ONLY_STRING "3.7.4"
 #define	ARCHIVE_VERSION_STRING "libarchive " ARCHIVE_VERSION_ONLY_STRING
 __LA_DECL const char *	archive_version_string(void);
 
