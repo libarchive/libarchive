364b97ea2a5516658c0d7d1323e008473ec980db
Tim Kientzle <kientzle@acm.org>
2024-07-05 15:40:14 -0700

    Remove some package updates that look unnecessary.

--- build/ci/github_actions/macos.sh
+++ build/ci/github_actions/macos.sh
@@ -2,9 +2,6 @@
 if [ "$1" = "prepare" ]
 then
 	set -x
-	brew uninstall openssl@1.1 > /dev/null
-	brew uninstall python@3.7 > /dev/null
-	brew untap local/openssl > /dev/null
 	brew update > /dev/null
 	brew upgrade > /dev/null
 	set -x -e
