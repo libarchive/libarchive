4ee35c1767083e69ecf93da5ee54918e4d537432
Tobias Stoeckmann <tobias@stoeckmann.org>
2024-08-12 23:21:54 +0200

    b64encode: Fix typo in error message
    If this function fails, log b64encode instead of uu.

--- libarchive/archive_write_add_filter_b64encode.c
+++ libarchive/archive_write_add_filter_b64encode.c
@@ -83,7 +83,7 @@ archive_write_add_filter_b64encode(struct archive *_a)
 	struct private_b64encode *state;
 
 	archive_check_magic(&a->archive, ARCHIVE_WRITE_MAGIC,
-	    ARCHIVE_STATE_NEW, "archive_write_add_filter_uu");
+	    ARCHIVE_STATE_NEW, "archive_write_add_filter_b64encode");
 
 	state = (struct private_b64encode *)calloc(1, sizeof(*state));
 	if (state == NULL) {
