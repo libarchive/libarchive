2ec6c6799f03b2735b2027c49ece93acd7a215af
zwimer <github@zwimer.com>
2024-10-04 08:27:55 -0400

    Increase length limits in archive_read_support_format_tar.c

--- libarchive/archive_read_support_format_tar.c
+++ libarchive/archive_read_support_format_tar.c
@@ -230,7 +230,7 @@ static void	tar_flush_unconsumed(struct archive_read *, size_t *);
  * need to be high enough for any correct value.  These
  * will likely need some adjustment as we get more experience. */
 static const size_t guname_limit = 65536; /* Longest uname or gname: 64kiB */
-static const size_t pathname_limit = 1048576; /* Longest path name: 1MiB */
+static const size_t pathname_limit = 4194304; /* Longest path name: 4MiB */
 static const size_t sparse_map_limit = 8 * 1048576; /* Longest sparse map: 8MiB */
 static const size_t xattr_limit = 16 * 1048576; /* Longest xattr: 16MiB */
 static const size_t fflags_limit = 512; /* Longest fflags */
