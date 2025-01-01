libarchive/libarchive

commit ab94a813b0f64cbc1bcb952bf55424a2d9c7f1d9

Use __LA_DEPRECATED for functions deprecated in 379867e
Reported-By:	Jamie Kenyon

libarchive/archive_entry.h
@@ -105,6 +105,12 @@ typedef int64_t la_int64_t;
 # define __LA_DECL
 #endif
 
+#if defined(__GNUC__) && __GNUC__ >= 3 && __GNUC_MINOR__ >= 1
+# define __LA_DEPRECATED __attribute__((deprecated))
+#else
+# define __LA_DEPRECATED
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -529,9 +535,9 @@ __LA_DECL int archive_entry_acl_from_text(struct archive_entry *,
 
 /* Deprecated functions */
 __LA_DECL const wchar_t	*archive_entry_acl_text_w(struct archive_entry *,
-		    int /* flags */) __attribute__ ((deprecated));
+		    int /* flags */) __LA_DEPRECATED;
 __LA_DECL const char *archive_entry_acl_text(struct archive_entry *,
-		    int /* flags */) __attribute__ ((deprecated));
+		    int /* flags */) __LA_DEPRECATED;
 
 /* Return bitmask of ACL types in an archive entry */
 __LA_DECL int	 archive_entry_acl_types(struct archive_entry *);

libarchive/archive_entry_locale.h
@@ -63,8 +63,7 @@ int _archive_entry_uname_l(struct archive_entry *,
     const char **, size_t *, struct archive_string_conv *);
 #define archive_entry_acl_text_l _archive_entry_acl_text_l
 int _archive_entry_acl_text_l(struct archive_entry *, int,
-const char **, size_t *, struct archive_string_conv *)
-__attribute__ ((deprecated));
+const char **, size_t *, struct archive_string_conv *) __LA_DEPRECATED;
 #define archive_entry_acl_to_text_l _archive_entry_acl_to_text_l
 char *_archive_entry_acl_to_text_l(struct archive_entry *, ssize_t *, int,
     struct archive_string_conv *);

commit 23f50df974f7725c7a0d7d0511031f2011aa44c7

Fix GCC version detection for __attribute__((deprecated))
The #if condition as-written fails for any major >= 3 if minor < 1, e.g.
GCC 15.0 (while in development).

Use the idiom described in the GCC docs [0] to avoid this.

[0] https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html

Fixes: ab94a813b0f64cbc1bcb952bf55424a2d9c7f1d9

libarchive/archive_entry.h
@@ -127,7 +127,8 @@ typedef ssize_t la_ssize_t;
 # define __LA_DECL
 #endif
 
-#if defined(__GNUC__) && __GNUC__ >= 3 && __GNUC_MINOR__ >= 1
+#if defined(__GNUC__) && (__GNUC__ > 3 || \
+    (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
 # define __LA_DEPRECATED __attribute__((deprecated))
 #else
 # define __LA_DEPRECATED