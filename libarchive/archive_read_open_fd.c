libarchive/libarchive

commit 85a8c85227016804e4453f07278967da2907e2d8

Eliminate a bunch of off_t uses.
Most of these are from the libarchive 2 API that's no longer supported.
A few are places where int64_t offsets need to be mapped down to system
APIs that might only be 32 bits; those are a little trickier.

SVN-Revision: 2937

libarchive/archive_read_open_fd.c
@@ -59,11 +59,7 @@ struct read_fd_data {
 
 static int	file_close(struct archive *, void *);
 static ssize_t	file_read(struct archive *, void *, const void **buff);
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t	file_skip(struct archive *, void *, off_t request);
-#else
 static int64_t	file_skip(struct archive *, void *, int64_t request);
-#endif
 
 int
 archive_read_open_fd(struct archive *a, int fd, size_t block_size)
@@ -126,19 +122,21 @@ file_read(struct archive *a, void *client_data, const void **buff)
 	}
 }
 
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t
-file_skip(struct archive *a, void *client_data, off_t request)
-#else
 static int64_t
 file_skip(struct archive *a, void *client_data, int64_t request)
-#endif
 {
 	struct read_fd_data *mine = (struct read_fd_data *)client_data;
+	off_t skip = (off_t)request;
 	off_t old_offset, new_offset;
+	int skip_bits = sizeof(skip) * 8 - 1;  /* off_t is a signed type. */
+	int64_t max_skip = (((int64_t)1 << (skip_bits - 1)) - 1) * 2 + 1;
 
 	if (!mine->use_lseek)
 		return (0);
+
+	/* Reduce a request that would overflow the 'skip' variable. */
+	if ((sizeof(request) > sizeof(skip)) && (request > max_skip))
+		skip = max_skip;
 
 	/* Reduce request to the next smallest multiple of block_size */
 	request = (request / mine->block_size) * mine->block_size;
@@ -146,7 +144,7 @@ file_skip(struct archive *a, void *client_data, int64_t request)
 		return (0);
 
 	if (((old_offset = lseek(mine->fd, 0, SEEK_CUR)) >= 0) &&
-	    ((new_offset = lseek(mine->fd, request, SEEK_CUR)) >= 0))
+	    ((new_offset = lseek(mine->fd, skip, SEEK_CUR)) >= 0))
 		return (new_offset - old_offset);
 
 	/* If seek failed once, it will probably fail again. */

libarchive/archive_read.c
@@ -276,9 +276,10 @@ static int64_t
 client_skip_proxy(struct archive_read_filter *self, int64_t request)
 {
 	int64_t ask, get, total;
-	/* Limit our maximum seek request to 1GB on platforms
-	* with 32-bit off_t (such as Windows). */
-	int64_t skip_limit = ((int64_t)1) << (sizeof(off_t) * 8 - 2);
+	/* Seek requests over 1GiB are broken down into multiple
+	 * seeks.  This avoids overflows when the requests get
+	 * passed through 32-bit arguments. */
+	int64_t skip_limit = (int64_t)1 << 30;
 
 	if (self->archive->client.skipper == NULL)
 		return (0);
@@ -629,7 +630,7 @@ archive_read_data(struct archive *_a, void *buff, size_t s)
 		}
 
 		/* Compute the amount of zero padding needed. */
-		if (a->read_data_output_offset + (off_t)s <
+		if (a->read_data_output_offset + s <
 		    a->read_data_offset) {
 			len = s;
 		} else if (a->read_data_output_offset <
@@ -690,11 +691,7 @@ archive_read_data_skip(struct archive *_a)
 	int r;
 	const void *buff;
 	size_t size;
-#if ARCHIVE_VERSION_NUMBER < 3000000
-	off_t offset;
-#else
 	int64_t offset;
-#endif
 
 	archive_check_magic(_a, ARCHIVE_READ_MAGIC, ARCHIVE_STATE_DATA,
 	    "archive_read_data_skip");

libarchive/archive_read_data_into_fd.c
@@ -85,11 +85,7 @@ archive_read_data_into_fd(struct archive *a, int fd)
 	const void *buff;
 	size_t size, bytes_to_write;
 	ssize_t bytes_written;
-#if ARCHIVE_VERSION_NUMBER < 3000000
-	off_t target_offset;
-#else
 	int64_t target_offset;
-#endif
 	int64_t actual_offset = 0;
 	int can_lseek;
 	char *nulls = NULL;

libarchive/archive_read_extract.c
@@ -138,11 +138,7 @@ archive_read_extract_set_progress_callback(struct archive *_a,
 static int
 copy_data(struct archive *ar, struct archive *aw)
 {
-#if ARCHIVE_VERSION_NUMBER < 3000000
-	off_t offset;
-#else
 	int64_t offset;
-#endif
 	const void *buff;
 	struct extract *extract;
 	size_t size;

libarchive/archive_read_open_file.c
@@ -59,11 +59,7 @@ struct read_FILE_data {
 
 static int	file_close(struct archive *, void *);
 static ssize_t	file_read(struct archive *, void *, const void **buff);
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t	file_skip(struct archive *, void *, off_t request);
-#else
 static int64_t	file_skip(struct archive *, void *, int64_t request);
-#endif
 
 int
 archive_read_open_FILE(struct archive *a, FILE *f)
@@ -119,15 +115,17 @@ file_read(struct archive *a, void *client_data, const void **buff)
 	return (bytes_read);
 }
 
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t
-file_skip(struct archive *a, void *client_data, off_t request)
-#else
 static int64_t
 file_skip(struct archive *a, void *client_data, int64_t request)
-#endif
 {
 	struct read_FILE_data *mine = (struct read_FILE_data *)client_data;
+#if HAVE_FSEEKO
+	off_t skip = (off_t)request;
+#else
+	long skip = (long)request;
+#endif
+	int skip_bits = sizeof(skip) * 8 - 1;
+	int64_t max_skip = (((int64_t)1 << (skip_bits - 1)) - 1) * 2 + 1;
 
 	(void)a; /* UNUSED */
 
@@ -140,10 +138,14 @@ file_skip(struct archive *a, void *client_data, int64_t request)
 	if (request == 0)
 		return (0);
 
+	/* If request is too big for a long or an off_t, reduce it. */
+	if ((sizeof(request) > sizeof(skip)) && (request > max_skip))
+		skip = max_skip;
+
 #if HAVE_FSEEKO
-	if (fseeko(mine->f, request, SEEK_CUR) != 0)
+	if (fseeko(mine->f, skip, SEEK_CUR) != 0)
 #else
-	if (fseek(mine->f, request, SEEK_CUR) != 0)
+	if (fseek(mine->f, skip, SEEK_CUR) != 0)
 #endif
 	{
 		mine->can_skip = 0;

libarchive/archive_read_open_filename.c
@@ -76,11 +76,7 @@ struct read_file_data {
 
 static int	file_close(struct archive *, void *);
 static ssize_t	file_read(struct archive *, void *, const void **buff);
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t	file_skip(struct archive *, void *, off_t request);
-#else
 static int64_t	file_skip(struct archive *, void *, int64_t request);
-#endif
 static off_t	file_skip_lseek(struct archive *, void *, off_t request);
 
 int
@@ -313,13 +309,8 @@ file_skip_lseek(struct archive *a, void *client_data, off_t request)
  * accelerate operation on tape drives.
  */
 
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t
-file_skip(struct archive *a, void *client_data, off_t request)
-#else
 static int64_t
 file_skip(struct archive *a, void *client_data, int64_t request)
-#endif
 {
 	struct read_file_data *mine = (struct read_file_data *)client_data;
 

libarchive/archive_read_open_memory.c
@@ -48,11 +48,7 @@ struct read_memory_data {
 
 static int	memory_read_close(struct archive *, void *);
 static int	memory_read_open(struct archive *, void *);
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t	memory_read_skip(struct archive *, void *, off_t request);
-#else
 static int64_t	memory_read_skip(struct archive *, void *, int64_t request);
-#endif
 static ssize_t	memory_read(struct archive *, void *, const void **buff);
 
 int
@@ -123,13 +119,8 @@ memory_read(struct archive *a, void *client_data, const void **buff)
  * necessary in order to better exercise internal code when used
  * as a test harness.
  */
-#if ARCHIVE_VERSION_NUMBER < 3000000
-static off_t
-memory_read_skip(struct archive *a, void *client_data, off_t skip)
-#else
 static int64_t
 memory_read_skip(struct archive *a, void *client_data, int64_t skip)
-#endif
 {
 	struct read_memory_data *mine = (struct read_memory_data *)client_data;
 

libarchive/archive_virtual.c
@@ -115,13 +115,8 @@ archive_write_data(struct archive *a, const void *buff, size_t s)
 	return ((a->vtable->archive_write_data)(a, buff, s));
 }
 
-#if ARCHIVE_VERSION_NUMBER < 3000000
-ssize_t
-archive_write_data_block(struct archive *a, const void *buff, size_t s, off_t o)
-#else
 ssize_t
 archive_write_data_block(struct archive *a, const void *buff, size_t s, int64_t o)
-#endif
 {
 	return ((a->vtable->archive_write_data_block)(a, buff, s, o));
 }
@@ -138,23 +133,9 @@ archive_read_next_header2(struct archive *a, struct archive_entry *entry)
 	return ((a->vtable->archive_read_next_header2)(a, entry));
 }
 
-#if ARCHIVE_VERSION_NUMBER < 3000000
-int
-archive_read_data_block(struct archive *a,
-    const void **buff, size_t *s, off_t *o)
-{
-	int r;
-	int64_t o64;
-
-	r = ((a->vtable->archive_read_data_block)(a, buff, s, &o64));
-	*o = (off_t)o64;
-	return (r);
-}
-#else
 int
 archive_read_data_block(struct archive *a,
     const void **buff, size_t *s, int64_t *o)
 {
 	return ((a->vtable->archive_read_data_block)(a, buff, s, o));
 }
-#endif

commit 276e5414d98067d79df746cdbcdf848ba559f19a

Fix file_skip block boundary regression
The request variable is reduced to next smallest multiple of a block
size but never used afterwards again. Assign the skip variable instead.

Fixes: 85a8c85227016804e4453f07278967da2907e2d8
Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

libarchive/archive_read_open_fd.c
@@ -146,9 +146,9 @@ file_skip(struct archive *a, void *client_data, int64_t request)
 			skip = (off_t)max_skip;
 	}
 
-	/* Reduce request to the next smallest multiple of block_size */
-	request = (request / mine->block_size) * mine->block_size;
-	if (request == 0)
+	/* Reduce 'skip' to the next smallest multiple of block_size */
+	skip = (off_t)(((int64_t)skip / mine->block_size) * mine->block_size);
+	if (skip == 0)
 		return (0);
 
 	if (((old_offset = lseek(mine->fd, 0, SEEK_CUR)) >= 0) &&