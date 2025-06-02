d7d1d19bb63ac9e234927dda420f17c37b76948f
Tobias Stoeckmann <tobias@stoeckmann.org>
2025-06-02 23:21:26 +0200

    windows: Preserve GetExitCodeProcess error
    If the waitpid version for Windows fails, preserve the error code and
    avoid overwriting it with a possible CloseHandle error.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- libarchive/archive_windows.c
+++ libarchive/archive_windows.c
@@ -641,8 +641,8 @@ __la_waitpid(HANDLE child, int *status, int option)
 	(void)option;/* UNUSED */
 	do {
 		if (GetExitCodeProcess(child, &cs) == 0) {
-			CloseHandle(child);
 			la_dosmaperr(GetLastError());
+			CloseHandle(child);
 			*status = 0;
 			return (-1);
 		}
