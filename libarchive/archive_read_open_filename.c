6aa31bacecc09f5f807f322841b45eaf07769617
Rose <gfunni234@gmail.com>
2025-06-04 18:10:20 -0400

    Free "mine" if the appending of the callback fails

--- libarchive/archive_read_open_filename.c
+++ libarchive/archive_read_open_filename.c
@@ -125,8 +125,10 @@ archive_read_open_filenames(struct archive *a, const char **filenames,
 			filename = "";
 		mine = calloc(1,
 			sizeof(*mine) + strlen(filename));
-		if (mine == NULL)
-			goto no_memory;
+		if (mine == NULL) {
+			archive_set_error(a, ENOMEM, "No memory");
+			return (ARCHIVE_FATAL);
+		}
 		strcpy(mine->filename.m, filename);
 		mine->block_size = block_size;
 		mine->fd = -1;
@@ -136,8 +138,10 @@ archive_read_open_filenames(struct archive *a, const char **filenames,
 			mine->filename_type = FNT_STDIN;
 		} else
 			mine->filename_type = FNT_MBS;
-		if (archive_read_append_callback_data(a, mine) != (ARCHIVE_OK))
+		if (archive_read_append_callback_data(a, mine) != (ARCHIVE_OK)) {
+			free(mine);
 			return (ARCHIVE_FATAL);
+		}
 		if (filenames == NULL)
 			break;
 		filename = *(filenames++);
@@ -150,9 +154,6 @@ archive_read_open_filenames(struct archive *a, const char **filenames,
 	archive_read_set_seek_callback(a, file_seek);
 
 	return (archive_read_open1(a));
-no_memory:
-	archive_set_error(a, ENOMEM, "No memory");
-	return (ARCHIVE_FATAL);
 }
 
 /*
@@ -178,8 +179,10 @@ archive_read_open_filenames_w(struct archive *a, const wchar_t **wfilenames,
 			wfilename = L"";
 		mine = calloc(1,
 			sizeof(*mine) + wcslen(wfilename) * sizeof(wchar_t));
-		if (mine == NULL)
-			goto no_memory;
+		if (mine == NULL) {
+			archive_set_error(a, ENOMEM, "No memory");
+			return (ARCHIVE_FATAL);
+		}
 		mine->block_size = block_size;
 		mine->fd = -1;
 
@@ -216,8 +219,10 @@ archive_read_open_filenames_w(struct archive *a, const wchar_t **wfilenames,
 			archive_string_free(&fn);
 #endif
 		}
-		if (archive_read_append_callback_data(a, mine) != (ARCHIVE_OK))
+		if (archive_read_append_callback_data(a, mine) != (ARCHIVE_OK)) {
+			free(mine);
 			return (ARCHIVE_FATAL);
+		}
 		if (wfilenames == NULL)
 			break;
 		wfilename = *(wfilenames++);
@@ -230,9 +235,6 @@ archive_read_open_filenames_w(struct archive *a, const wchar_t **wfilenames,
 	archive_read_set_seek_callback(a, file_seek);
 
 	return (archive_read_open1(a));
-no_memory:
-	archive_set_error(a, ENOMEM, "No memory");
-	return (ARCHIVE_FATAL);
 }
 
 int
