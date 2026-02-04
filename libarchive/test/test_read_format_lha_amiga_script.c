a8f8716a43babd69b857e72be6cfd536608881f9
Alain Greppin <agreppin@chilibi.ch>
2026-02-05 00:52:16 +0100

    PR#2842 add missing test_read_format_lha_amiga_script.lha.uu to Makefile.am

--- libarchive/test/test_read_format_lha_amiga_script.c
+++ libarchive/test/test_read_format_lha_amiga_script.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2014 Michihiro NAKAJIMA
+ * Copyright (c) 2026 Alain Greppin
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
