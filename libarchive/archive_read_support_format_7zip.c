d598dc1ce51c5a09020e57323ea121c26eb43504
Dustin L. Howett <dustin@howett.net>
2026-07-01 12:01:06 -0500

    Merge pull request #3218 from datauwu/7zip-redundant-bcj-assign
    7zip: drop redundant BCJ2 assignments
    (cherry picked from commit d586479a7284c296e6a27da92d66d737d2064ae0)

--- libarchive/archive_read_support_format_7zip.c
+++ libarchive/archive_read_support_format_7zip.c
@@ -3953,15 +3953,11 @@ setup_decode_folder(struct archive_read *a, struct _7z_folder *folder,
 				}
 			}
 			coder2 = &(fc[3]);
-			zip->main_stream_bytes_remaining =
-				folder->unPackSize[2];
 			remaining = folder->unPackSize[2];
 		} else if (coder2 != NULL && coder2->codec == _7Z_X86_BCJ2 &&
 		    zip->pack_stream_remaining == 4 &&
 		    folder->numInStreams == 5 && folder->numOutStreams == 2) {
 			/* Source type 0 made by 7z */
-			zip->main_stream_bytes_remaining =
-				folder->unPackSize[0];
 			remaining = folder->unPackSize[0];
 		} else {
 			/* We got an unexpected form. */
