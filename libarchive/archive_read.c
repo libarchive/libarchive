1cafc7ab965f58f630d8f9e7ca14c3836d1730e8
Dustin L. Howett <dustin@howett.net>
2026-07-01 16:04:34 -0500

    Merge pull request #3221 from stoeckmann/callback_data_check
    (cherry picked from commit 52bf591c1a7c0d6dabc7f85ec60130ec7e0f42c6)

--- libarchive/archive_read.c
+++ libarchive/archive_read.c
@@ -135,12 +135,16 @@ archive_read_open(struct archive *a, void *client_data,
     archive_open_callback *client_opener, archive_read_callback *client_reader,
     archive_close_callback *client_closer)
 {
+	int r;
+
 	/* Old archive_read_open() is just a thin shell around
 	 * archive_read_open1. */
 	archive_read_set_open_callback(a, client_opener);
 	archive_read_set_read_callback(a, client_reader);
 	archive_read_set_close_callback(a, client_closer);
-	archive_read_set_callback_data(a, client_data);
+	r = archive_read_set_callback_data(a, client_data);
+	if (r < 0)
+		return (r);
 	return archive_read_open1(a);
 }
 
@@ -152,9 +156,13 @@ archive_read_open2(struct archive *a, void *client_data,
     archive_skip_callback *client_skipper,
     archive_close_callback *client_closer)
 {
+	int r;
+
 	/* Old archive_read_open2() is just a thin shell around
 	 * archive_read_open1. */
-	archive_read_set_callback_data(a, client_data);
+	r = archive_read_set_callback_data(a, client_data);
+	if (r < 0)
+		return (r);
 	archive_read_set_open_callback(a, client_opener);
 	archive_read_set_read_callback(a, client_reader);
 	archive_read_set_skip_callback(a, client_skipper);
