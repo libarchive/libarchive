fa383310293f71beb894800b2c4210bb2217c2b1
Mike Frysinger <vapier@gmail.com>
2026-08-18 19:34:58 +0700

    check HAVE_TZSET
    The build system already checks for tzset dynamically,
    so respect the test in the code that uses the function.

--- libarchive/archive_write_set_format_iso9660.c
+++ libarchive/archive_write_set_format_iso9660.c
@@ -2525,7 +2525,9 @@ get_tmfromtime(struct tm *tm, time_t *t)
 #if HAVE_LOCALTIME_S
 	localtime_s(tm, t);
 #elif HAVE_LOCALTIME_R
+# ifdef HAVE_TZSET
 	tzset();
+# endif
 	localtime_r(t, tm);
 #else
 	memcpy(tm, localtime(t), sizeof(*tm));
